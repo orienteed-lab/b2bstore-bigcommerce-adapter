f813ce1349c0e9e7bfd0c75e23e5e0e3471015d4
dggorienteed <dgonzalez@orienteed.com>
2023-04-24 08:18:19 +0200

    update GetProductListing resolver

--- src/resolvers/cart/GetProductListing/getProductListing.ts
+++ src/resolvers/cart/GetProductListing/getProductListing.ts
@@ -1,10 +1,64 @@
 import { ClientProps } from 'src';
 import { GetProductListingQueryVariables } from '@schema';
 
-const GetProductListing = (clientProps: ClientProps) => (resolverProps: GetProductListingQueryVariables) => {
-    // Look docs for more info about how to fill this function
+import { getProductListingParser } from './getProductListingParser';
+import { useEffect, useState } from 'react';
 
-    return { data: {}, loading: false, error: undefined };
+interface GetCartDetailsProps {
+    variables: GetProductListingQueryVariables;
+}
+
+const GetProductListing = (clientProps: ClientProps) => (resolverProps: GetCartDetailsProps) => {
+    const { restClient } = clientProps;
+    const {
+        variables: { cartId }
+    } = resolverProps;
+
+
+    console.log({ cartId })
+
+    const [loading, setLoading] = useState(true);
+    const [data, setData] = useState<any>(null);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                const rawData = await restClient(
+                    `/api/v3/carts/${cartId}?include=line_items.physical_items.options,line_items.digital_items.options`,
+                    {
+                        method: 'GET',
+                        headers: {
+                            backendTechnology: 'bigcommerce'
+                        }
+                    }
+                );
+                // setData(generateTokenParser(rawData));
+                setData(rawData);
+            } catch (err: any) {
+                setError(err);
+            }
+            setLoading(false);
+        };
+        fetchData();
+    }, []);
+
+    console.log('antes del parser', data);
+
+    let parsedData = undefined;
+    if (data) {
+        // try {
+        parsedData = getProductListingParser(data);
+        // } catch (e) {
+        //     console.error(e);
+        // }
+    }
+
+    console.log('despues del parser', parsedData);
+
+    //return { data, loading, error };
+    return { data: parsedData, loading, error };
 };
 
 export default GetProductListing;
