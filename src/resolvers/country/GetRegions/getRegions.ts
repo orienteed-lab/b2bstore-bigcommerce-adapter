997e367e8d16526a71bdd9d027a8c0581174d011
igonzalez <igonzalez@orienteed.com>
2023-04-25 15:55:46 +0200

    Update getRegions resolver

--- src/resolvers/country/GetRegions/getRegions.ts
+++ src/resolvers/country/GetRegions/getRegions.ts
@@ -1,10 +1,67 @@
 import { ClientProps } from 'src';
-import { GetRegionsQueryVariables } from '@schema';
+import { GetCountriesQueryVariables, GetRegionsQueryVariables } from '@schema';
 
-const GetRegions = (clientProps: ClientProps) => (resolverProps: GetRegionsQueryVariables) => {
-    // Look docs for more info about how to fill this function
+import { getRegionsParser } from './getRegionsParser';
+import { useEffect, useState } from 'react';
 
-    return { data: {}, loading: false, error: undefined };
+interface GetRegionsProps {
+    variables: GetRegionsQueryVariables;
+}
+
+const GetRegions = (clientProps: ClientProps) => (resolverProps: GetRegionsProps) => {
+    const { restClient, useQuery, mergeOperations } = clientProps;
+    const {
+        variables: { countryCode: country_iso2 }
+    } = resolverProps;
+
+    const [loading, setLoading] = useState(true);
+    const [data, setData] = useState<any>(null);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            if (country_iso2) {
+                setLoading(true);
+                try {
+                    restClient(`/api/v2/countries?country_iso2=${country_iso2}`, {
+                        method: 'GET',
+                        headers: {
+                            backendTechnology: 'bigcommerce'
+                        }
+                    }).then((country) => {
+                        console.log('Country', country);
+                        restClient(`/api/v2/countries/${country[0].id}/states`, {
+                            method: 'GET',
+                            headers: {
+                                backendTechnology: 'bigcommerce'
+                            }
+                        }).then((rawData) => {
+                            setData(rawData);
+                        });
+                    });
+                } catch (err: any) {
+                    setError(err);
+                }
+                setLoading(false);
+            }
+        };
+        fetchData();
+    }, [country_iso2]);
+
+    console.log('antes del parser', data);
+
+    let parsedData = undefined;
+    if (data) {
+        // try {
+        parsedData = getRegionsParser(data);
+        // } catch (e) {
+        //     console.error(e);
+        // }
+    }
+
+    console.log('despues del parser', parsedData);
+
+    return { data: parsedData, loading, error };
 };
 
 export default GetRegions;
--- src/resolvers/country/GetRegions/getRegionsParser.ts
+++ src/resolvers/country/GetRegions/getRegionsParser.ts
@@ -1,6 +1,18 @@
 import { GetRegionsQuery } from '@schema';
 
-export const getRegionsParser = (data: any): GetRegionsQuery => {
-    // Your parser logic here
-    return data;
+export const getRegionsParser = ( data: any): GetRegionsQuery => {
+
+    return {
+        country: {
+            id: data[0].country_id,
+            available_regions: data.map((reg: any) => ({
+                id: reg.id,
+                code: reg.state_abbreviation,
+                name: reg.state
+            }))
+        }
+    };
 };
+
+
+
