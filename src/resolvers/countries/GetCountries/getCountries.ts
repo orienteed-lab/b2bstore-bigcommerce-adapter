d16f953a75060020a62731e3fb1288591897f688
igonzalez <igonzalez@orienteed.com>
2023-04-25 16:32:13 +0200

    Update GetCountries resolver

--- src/resolvers/countries/GetCountries/getCountries.ts
+++ src/resolvers/countries/GetCountries/getCountries.ts
@@ -1,9 +1,64 @@
 import { ClientProps } from 'src';
+import { GetCountriesQueryVariables } from '@schema';
 
-const GetCountries = (clientProps: ClientProps) => () => {
-    // Look docs for more info about how to fill this function
+import { getCountriesParser } from './getCountriesParser';
+import { useEffect, useState } from 'react';
 
-    return { data: {}, loading: false, error: undefined };
+const GetCountries = (clientProps: ClientProps) => (resolverProps: GetCountriesQueryVariables) => {
+    const { restClient, useQuery, mergeOperations } = clientProps;
+
+    const [loading, setLoading] = useState(true);
+    const [data, setData] = useState<any>(null);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+                restClient(
+                    `/api/v2/countries/count`,
+                    {
+                        method: 'GET',
+                        headers: {
+                            backendTechnology: 'bigcommerce'
+                        }
+                    }
+                ).then((n) => {
+                    restClient(
+                        `/api/v2/countries?limit=${n.count}`,
+                        {
+                            method: 'GET',
+                            headers: {
+                                backendTechnology: 'bigcommerce'
+                            }
+                        }
+                    ).then((rawData) => {
+                        setData(rawData);
+                    });
+                }).catch((err) => {
+                    setError(err)
+                }).finally(() =>{
+                    setLoading(false);
+                });
+        }
+        fetchData();
+    }, []);
+
+    console.log('antes del parser', data);
+
+    let parsedData = undefined;
+    if (data) {
+        // try {
+        parsedData = getCountriesParser(data);
+        // } catch (e) {
+        //     console.error(e);
+        // }
+    }
+
+    console.log('despues del parser', parsedData);
+
+
+    return { data: parsedData, loading, error};
+    // return { data, loading, error};
 };
 
 export default GetCountries;
--- src/resolvers/countries/GetCountries/getCountriesParser.ts
+++ src/resolvers/countries/GetCountries/getCountriesParser.ts
@@ -1,6 +1,11 @@
 import { GetCountriesQuery } from '@schema';
 
 export const getCountriesParser = (data: any): GetCountriesQuery => {
-    // Your parser logic here
-    return data;
+    return {
+    countries: data.map((co: any) => ({
+        id: co.country_iso2,
+        full_name_english: co.country,
+        two_letter_abbreviation: co.country_iso2
+    }))
+};
 };
