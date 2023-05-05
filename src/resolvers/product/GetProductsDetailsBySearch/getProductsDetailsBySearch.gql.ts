3f6a4bba19c57446acd03af34c4c65e69cfaf1b4
igonzalez <igonzalez@orienteed.com>
2023-05-05 13:49:08 +0200

    update getProductDetailsBySearch resolver

--- src/resolvers/product/GetProductsDetailsBySearch/getProductsDetailsBySearch.gql.ts
+++ src/resolvers/product/GetProductsDetailsBySearch/getProductsDetailsBySearch.gql.ts
@@ -1,7 +1,127 @@
 import { gql } from '@apollo/client';
 
 export const GET_PRODUCTS_DETAILS_BY_SEARCH = gql`
-    # Your query here
+    query getProductDetailsBySearch($filters: SearchProductsFiltersInput!, $sort: SearchProductsSortInput) {
+        site {
+            search {
+                searchProducts(filters: $filters, sort: $sort) {
+                    products {
+                        collectionInfo {
+                            totalItems
+                        }
+                        edges {
+                            node {
+                                id
+                                entityId
+                                name
+                                sku
+                                description
+                                categories{
+                                    edges{
+                                        node{
+                                            name
+                                        }
+                                    }
+                                }
+                                inventory {
+                                    isInStock
+                                }
+                                reviewSummary {
+                                    summationOfRatings
+                                }
+                                type
+                                path
+                                productOptions {
+                                    edges {
+                                        node {
+                                            __typename
+                                            displayName
+                                            entityId
+                                            isRequired
+                                            isVariantOption
+                                        }
+                                    }
+                                }
+                                variants {
+                                    edges {
+                                        node {
+                                            sku
+                                            entityId
+                                            inventory {
+                                                isInStock
+                                            }
+                                            upc
+                                            options {
+                                                edges {
+                                                    node {
+                                                        displayName
+                                                        entityId
+                                                        values {
+                                                            edges {
+                                                                node {
+                                                                    entityId
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                            prices {
+                                                price {
+                                                    currencyCode
+                                                    value
+                                                }
+                                                priceRange{
+                                                    min{
+                                                        currencyCode
+                                                        value
+                                                    }
+                                                    max{
+                                                        currencyCode
+                                                        value
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                                defaultImage{
+                                    urlOriginal
+                                }
+                                customFields {
+                                    edges {
+                                        node {
+                                            name
+                                            value
+                                        }
+                                    }
+                                }
+                                prices {
+                                    basePrice {
+                                        currencyCode
+                                        value
+                                    }
+                                    priceRange {
+                                        min {
+                                            currencyCode
+                                            value
+                                        }
+                                        max {
+                                            currencyCode
+                                            value
+                                        }
+                                    }
+                                }
+                                defaultImage {
+                                    url(width: 320)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 `;
 
 export default {
--- src/resolvers/product/GetProductsDetailsBySearch/getProductsDetailsBySearch.ts
+++ src/resolvers/product/GetProductsDetailsBySearch/getProductsDetailsBySearch.ts
@@ -1,10 +1,112 @@
 import { ClientProps } from 'src';
 import { GetProductsDetailsBySearchQueryVariables } from '@schema';
+import { useEffect } from 'react';
+
+import { getProductsDetailsBySearchParser } from './getProductsDetailsBySearchParser';
+import DEFAULT_OPERATIONS from './getProductsDetailsBySearch.gql';
 
 const GetProductsDetailsBySearch = (clientProps: ClientProps) => (resolverProps: GetProductsDetailsBySearchQueryVariables) => {
-    // Look docs for more info about how to fill this function
+    const { useQuery, mergeOperations, useLazyQuery } = clientProps;
+    const { inputText, pageSize, currentPage, filters, sort } = resolverProps;
 
-    return { data: {}, loading: false, error: undefined };
+    const operations = mergeOperations(DEFAULT_OPERATIONS);
+    const { getProductsDetailsBySearchQuery } = operations;
+
+    // const sortReference = {
+    //     'name:asc': 'A_TO_Z',
+    //     'name:desc': 'Z_TO_A',
+    //     'price:asc': 'LOW_TO_HIGH',
+    //     'price:desc': 'HIGH_TO_LOW',
+    // }
+
+    // const [runQuery, queryResponse] = useLazyQuery(getProductsDetailsBySearchQuery, {
+    //     fetchPolicy: 'cache-and-network',
+    //     nextFetchPolicy: 'cache-first'
+    // });
+
+    // const { called, loading, error, data } = queryResponse;
+
+     let parsedData = undefined;
+
+    // useEffect(() => {
+    //     runQuery({
+    //         variables: {
+    //             filters: {
+    //                 searchTerm: inputText,
+    //                 productAttributes: [
+    //                     {
+    //                         attribute: 'Color',
+    //                         values: ['Plata']
+    //                     }
+    //                 ]
+    //             },
+    //             sort: 'A_TO_Z'
+    //         }
+    //     });
+
+
+
+
+
+
+
+
+    const { data, loading, error, refetch } = useQuery(getProductsDetailsBySearchQuery, {
+        context: {
+            headers: {
+                backendTechnology: ['bigcommerce']
+            }
+        },
+        variables: {
+            filters: {
+                                searchTerm: inputText,
+                                productAttributes: [
+                                    {
+                                        attribute: 'Color',
+                                        values: ['Plata']
+                                    }
+                                ]
+                            },
+                            sort: 'A_TO_Z'
+        }
+    });
+
+
+
+
+
+        //console.log('antes del parser', data);
+
+        if (data) {
+            // try {
+            //parsedData = getProductsDetailsBySearchParser(data, currentPage, pageSize);
+            parsedData = getProductsDetailsBySearchParser(data);
+            // } catch (e) {
+            //     console.error(e);
+            // }
+        }
+        //console.log('despues del parser', parsedData);
+        return { data: parsedData, loading, error };
+    
+    //, [runQuery, currentPage, pageSize, data]);
+
+  
+
+    // const { data, loading, error } = useQuery(getCategoryQuery, {
+    //     context: {
+    //         headers: {
+    //             backendTechnology: ['bigcommerce']
+    //         }
+    //     },
+    //     variables: {
+    //         id: id,
+    //         filters: filters,
+    //         // sort: sortReference[sort]
+    //         sort: "A_TO_Z", //Use the BigCommerce sort options. Set them in the storefront
+    //     }
+    // });
+
+
 };
 
 export default GetProductsDetailsBySearch;
