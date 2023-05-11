bf46719df8a8bc9931497ef12fa884d7a2cb4308
igonzalez <igonzalez@orienteed.com>
2023-05-11 10:14:22 +0200

    update getCheckoutDetails resolver

--- src/resolvers/cart/GetCheckoutDetails/getCheckoutDetails.ts
+++ src/resolvers/cart/GetCheckoutDetails/getCheckoutDetails.ts
@@ -1,10 +1,63 @@
 import { ClientProps } from 'src';
 import { GetCheckoutDetailsQueryVariables } from '@schema';
 
-const GetCheckoutDetails = (clientProps: ClientProps) => (resolverProps: GetCheckoutDetailsQueryVariables) => {
-    // Look docs for more info about how to fill this function
+import { getCheckoutDetailsParser } from './getCheckoutDetailsParser';
+import { useEffect, useState } from 'react';
 
-    return { data: {}, loading: false, error: undefined };
+const GetCheckoutDetails = (clientProps: ClientProps) => (resolverProps: GetCheckoutDetailsQueryVariables) => {
+    const { restClient } = clientProps;
+    const { cartId }  = resolverProps;
+ 
+    const [loading, setLoading] = useState(true);
+    const [data, setData] = useState<any>(null);
+    const [data2, setData2] = useState<any>(null);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                const rawData = await restClient(
+                    `/api/v3/checkouts/${cartId}?include=cart.line_items.physical_items.options,cart.line_items.digital_items.options`,
+                    {
+                        method: 'GET',
+                        headers: {
+                            backendTechnology: 'bigcommerce'
+                        }
+                    }
+                );
+                const rawData2 = await restClient(
+                    `/api/v2/payments/methods`,
+                    {
+                        method: 'GET',
+                        headers: {
+                            backendTechnology: 'bigcommerce'
+                        }
+                    }
+                );
+                setData(rawData);
+                setData2(rawData2);
+            } catch (err: any) {
+                setError(err);
+            }
+            setLoading(false);
+        };
+        fetchData();
+    }, []);
+
+
+    let parsedData = undefined;
+    if (data) {
+        // try {
+        parsedData = getCheckoutDetailsParser(data, data2);
+        // } catch (e) {
+        //     console.error(e);
+        // }
+    }
+
+
+    return { data: parsedData, loading, error };
+
 };
 
 export default GetCheckoutDetails;
--- src/resolvers/cart/GetCheckoutDetails/getCheckoutDetailsParser.ts
+++ src/resolvers/cart/GetCheckoutDetails/getCheckoutDetailsParser.ts
@@ -1,6 +1,109 @@
+import { iterateObserversSafely } from '@apollo/client/utilities';
 import { GetCheckoutDetailsQuery } from '@schema';
+import GetItemCount from '../GetItemCount/getItemCount';
 
-export const getCheckoutDetailsParser = (data: any): GetCheckoutDetailsQuery => {
-    // Your parser logic here
-    return data;
+export const getCheckoutDetailsParser = ({data}: any, data2): GetCheckoutDetailsQuery => {
+    const total_quantity = () => {
+        var total = 0;
+        if (data.cart.line_items.physical_items) {
+            data.cart.line_items.physical_items.map((item: any) => {
+                total = total + item.quantity;
+            });
+        }
+        if (data.cart.line_items.digital_items) {
+            data.cart.line_items.digital_items.map((item: any) => {
+                total = total + item.quantity;
+            });
+        }
+        if (data.cart.line_items.custom_items) {
+            data.cart.line_items.custom_items.map((item: any) => {
+                total = total + item.quantity;
+            });
+        }
+        return total;
+    };
+
+    const getItems = () => {
+        var items = [];
+
+        if (data.cart.line_items.physical_items) {
+            data.cart.line_items.physical_items.map((item: any) => {
+                item.options.length !== 0
+                    ? items.push({
+                          __typename: 'ConfigurableCartItem',
+                          uid: item.id,
+                          product: {
+                              __typename: 'ConfigurableProduct',
+                              uid: item.id,
+                              stock_status: 'IN_STOCK' //If there is no stock you can't add it to cart
+                          }
+                      })
+                    : items.push({
+                          __typename: 'SimpleItem',
+                          uid: item.id,
+                          product: {
+                              __typename: 'SimpleProduct',
+                              uid: item.id,
+                              stock_status: 'IN_STOCK' //If there is no stock you can't add it to cart
+                          }
+                      });
+            });
+        }
+
+        if (data.cart.line_items.digital_items) {
+            data.cart.line_items.digital_items.map((item: any) => {
+                item.options.length !== 0
+                    ? items.push({
+                        __typename: 'ConfigurableCartItem',
+                        uid: item.id,
+                        product: {
+                            __typename: 'ConfigurableProduct',
+                            uid: item.id,
+                            stock_status: 'IN_STOCK' //If there is no stock you can't add it to cart
+                        }
+                    })
+                  : items.push({
+                        __typename: 'SimpleItem',
+                        uid: item.id,
+                        product: {
+                            __typename: 'SimpleProduct',
+                            uid: item.id,
+                            stock_status: 'IN_STOCK' //If there is no stock you can't add it to cart
+                          }
+                      });
+            });
+        }
+
+        if (data.cart.line_items.custom_items) {
+            data.cart.line_items.custom_items.map((item: any) => {
+  
+                   items.push({
+                        __typename: 'SimpleItem',
+                        uid: item.id,
+                        product: {
+                            __typename: 'SimpleProduct',
+                            uid: item.id,
+                            stock_status: 'IN_STOCK' //If there is no stock you can't add it to cart
+                          }
+                      });
+            });
+        }
+        return items;
+    };
+
+    return {
+        cart: {
+            __typename: 'Cart',
+            id: data.id,
+
+            total_quantity: total_quantity(),
+
+            items: getItems(),
+
+            available_payment_methods: data2.map((pay: any) => ({
+                __typename: pay.type,
+                code: pay.code
+            }))
+        }
+    };
 };
