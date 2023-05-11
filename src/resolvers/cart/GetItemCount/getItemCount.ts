2e6f5f03971ca9e87dd39e5a3ee1e8ca3ee2d079
igonzalez <igonzalez@orienteed.com>
2023-05-11 16:16:34 +0200

    update getItemCount resolver

--- src/resolvers/cart/GetItemCount/getItemCount.ts
+++ src/resolvers/cart/GetItemCount/getItemCount.ts
@@ -1,10 +1,47 @@
 import { ClientProps } from 'src';
 import { GetItemCountQueryVariables } from '@schema';
 
+import { getItemCountParser } from './getItemCountParser';
+import { useEffect, useState } from 'react';
 const GetItemCount = (clientProps: ClientProps) => (resolverProps: GetItemCountQueryVariables) => {
-    // Look docs for more info about how to fill this function
+    const { restClient } = clientProps;
+    const { cartId }  = resolverProps;
+ 
+    const [loading, setLoading] = useState(true);
+    const [data, setData] = useState<any>(null);
+    const [error, setError] = useState(null);
 
-    return { data: {}, loading: false, error: undefined };
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                const rawData = await restClient(
+                    `/api/v3/carts/${cartId}?include=line_items.physical_items.options,line_items.digital_items.options`,
+                    {
+                        method: 'GET',
+                        headers: {
+                            backendTechnology: 'bigcommerce'
+                        }
+                    }
+                );
+                
+                setData(rawData);
+            } catch (err: any) {
+                setError(err);
+            }
+            setLoading(false);
+        };
+        fetchData();
+    }, []);
+
+
+    let parsedData = undefined;
+    if (data) {
+
+        parsedData = getItemCountParser(data);
+
+    }
+    return { data: parsedData, loading, error };
 };
 
 export default GetItemCount;
--- src/resolvers/cart/GetItemCount/getItemCountParser.ts
+++ src/resolvers/cart/GetItemCount/getItemCountParser.ts
@@ -1,6 +1,33 @@
 import { GetItemCountQuery } from '@schema';
 
-export const getItemCountParser = (data: any): GetItemCountQuery => {
-    // Your parser logic here
-    return data;
+export const getItemCountParser = ({data}: any): GetItemCountQuery => {
+    
+    const total_quantity = () => {
+        var total = 0;
+        if (data.line_items.physical_items) {
+            data.line_items.physical_items.map((item: any) => {
+                total = total + item.quantity;
+            });
+        }
+        if (data.line_items.digital_items) {
+            data.line_items.digital_items.map((item: any) => {
+                total = total + item.quantity;
+            });
+        }
+        if (data.line_items.custom_items) {
+            data.line_items.custom_items.map((item: any) => {
+                total = total + item.quantity;
+            });
+        }
+        return total;
+    };
+
+    return {
+        cart:{
+            __typename: 'Cart',
+            id: data.Id,
+            total_quantity: total_quantity()
+
+        }
+    };
 };
