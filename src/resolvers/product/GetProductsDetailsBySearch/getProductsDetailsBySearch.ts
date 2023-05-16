2b6adb733882d4c0c694e298eae6f61f7b6e252e
dggorienteed <dgonzalez@orienteed.com>
2023-05-16 13:28:45 +0200

    Fix some issues in getProductsDetailsBySearch

--- src/resolvers/product/GetProductsDetailsBySearch/getProductsDetailsBySearch.ts
+++ src/resolvers/product/GetProductsDetailsBySearch/getProductsDetailsBySearch.ts
@@ -1,111 +1,40 @@
 import { ClientProps } from 'src';
 import { GetProductsDetailsBySearchQueryVariables } from '@schema';
-import { useEffect } from 'react';
 
 import { getProductsDetailsBySearchParser } from './getProductsDetailsBySearchParser';
 import DEFAULT_OPERATIONS from './getProductsDetailsBySearch.gql';
 
 const GetProductsDetailsBySearch = (clientProps: ClientProps) => (resolverProps: GetProductsDetailsBySearchQueryVariables) => {
-    const { useQuery, mergeOperations, useLazyQuery } = clientProps;
-    const { inputText, pageSize, currentPage, filters, sort } = resolverProps;
+    const { mergeOperations, useLazyQuery } = clientProps;
+    const { pageSize } = resolverProps;
 
     const operations = mergeOperations(DEFAULT_OPERATIONS);
     const { getProductsDetailsBySearchQuery } = operations;
 
-    // const sortReference = {
-    //     'name:asc': 'A_TO_Z',
-    //     'name:desc': 'Z_TO_A',
-    //     'price:asc': 'LOW_TO_HIGH',
-    //     'price:desc': 'HIGH_TO_LOW',
-    // }
-
-    // const [runQuery, queryResponse] = useLazyQuery(getProductsDetailsBySearchQuery, {
-    //     fetchPolicy: 'cache-and-network',
-    //     nextFetchPolicy: 'cache-first'
-    // });
-
-    // const { called, loading, error, data } = queryResponse;
-
-     let parsedData = undefined;
-
-    // useEffect(() => {
-    //     runQuery({
-    //         variables: {
-    //             filters: {
-    //                 searchTerm: inputText,
-    //                 productAttributes: [
-    //                     {
-    //                         attribute: 'Color',
-    //                         values: ['Plata']
-    //                     }
-    //                 ]
-    //             },
-    //             sort: 'A_TO_Z'
-    //         }
-    //     });
-
-
-
-
-
-
-
-
-    const { data, loading, error, refetch } = useQuery(getProductsDetailsBySearchQuery, {
-        context: {
-            headers: {
-                backendTechnology: ['bigcommerce']
+    const [runQuery, { called, loading, error, data }] = useLazyQuery(
+        getProductsDetailsBySearchQuery,
+        {
+            fetchPolicy: 'cache-and-network',
+            nextFetchPolicy: 'cache-first',
+            context: {
+                headers: {
+                    backendTechnology: ['bigcommerce']
+                }
             }
-        },
-        variables: {
-            filters: {
-                                searchTerm: inputText,
-                                productAttributes: [
-                                    {
-                                        attribute: 'Color',
-                                        values: ['Plata']
-                                    }
-                                ]
-                            },
-                            sort: 'A_TO_Z'
         }
-    });
+    );
 
+    let parsedData = undefined;
 
-
-
-
-        //console.log('antes del parser', data);
-
-        if (data) {
-            // try {
-            //parsedData = getProductsDetailsBySearchParser(data, currentPage, pageSize);
-            parsedData = getProductsDetailsBySearchParser(data);
-            // } catch (e) {
-            //     console.error(e);
-            // }
+    if (data) {
+        try {
+        parsedData = getProductsDetailsBySearchParser(data, pageSize); //TODO_B2B: Check how to use currentPage var
+        } catch (e) {
+            console.error(e);
         }
-        //console.log('despues del parser', parsedData);
-        return { data: parsedData, loading, error };
-    
-    //, [runQuery, currentPage, pageSize, data]);
+    }
 
-  
-
-    // const { data, loading, error } = useQuery(getCategoryQuery, {
-    //     context: {
-    //         headers: {
-    //             backendTechnology: ['bigcommerce']
-    //         }
-    //     },
-    //     variables: {
-    //         id: id,
-    //         filters: filters,
-    //         // sort: sortReference[sort]
-    //         sort: "A_TO_Z", //Use the BigCommerce sort options. Set them in the storefront
-    //     }
-    // });
-
+    return { runQuery, called, data: parsedData, loading, error };
 
 };
 
