95e8ef8bee5233a063e79e18d511da4ae1aeed8f
dggorienteed <dgonzalez@orienteed.com>
2023-07-12 10:50:02 +0200

    Update createAccount resolver

--- src/resolvers/customer/CreateAccount/createAccount.ts
+++ src/resolvers/customer/CreateAccount/createAccount.ts
@@ -1,10 +1,40 @@
 import { ClientProps } from 'src';
 import { CreateAccountMutationVariables } from '@schema';
+import { useState } from 'react';
 
-const CreateAccount = (clientProps: ClientProps) => (resolverProps: CreateAccountMutationVariables) => {
-    // Look docs for more info about how to fill this function
+const CreateAccount = (clientProps: ClientProps) => (resolverProps: CreateAccountMutationVariables = {email: "", firstname: "", is_subscribed: null, lastname: "", password: ""}) => {
+    const { restClient } = clientProps;
+    const [error, setError] = useState(undefined);
 
-    return { data: {}, loading: false, error: undefined };
+    const createAccount = async ({variables}) => {
+        const data = [
+            {
+                email: variables.email,
+                first_name: variables.firstname,
+                last_name: variables.lastname,
+                authentication: {
+                    force_password_reset: false,
+                    new_password: variables.password
+                },
+                accepts_product_review_abandoned_cart_emails: variables.is_subscribed,
+                origin_channel_id: 1,
+                channel_ids: [1]
+            }
+        ];
+        try {
+            await restClient(`/api/v3/customers`, {
+                method: 'POST',
+                headers: {
+                    backendTechnology: ['bigcommerce']
+                },
+                body: JSON.stringify(data)
+            });
+        } catch (err) {
+            setError(err);
+        }
+    };
+
+    return { createAccount, error };
 };
 
 export default CreateAccount;
