fb834bcc581b642efab1e985ccd74b145611f809
igonzalez <igonzalez@orienteed.com>
2023-07-14 11:40:51 +0200

    update subscribeToNewsletter resolver

--- src/resolvers/customer/newsletter/SubscribeToNewsletter/subscribeToNewsletter.ts
+++ src/resolvers/customer/newsletter/SubscribeToNewsletter/subscribeToNewsletter.ts
@@ -1,9 +1,34 @@
 import { ClientProps } from 'src';
+import { SubscribeToNewsletterMutationVariables } from '@schema';
+import { subscribeToNewsletterParser } from './subscribeToNewsletterParser';
+import { useState } from 'react';
 
-const SubscribeToNewsletter = (clientProps: ClientProps) => () => {
-    // Look docs for more info about how to fill this function
+const SubscribeToNewsletter = (clientProps: ClientProps) => (resolverProps: SubscribeToNewsletterMutationVariables ) => {
 
-    return { data: {}, loading: false, error: undefined };
+    const { restClient } = clientProps;
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const [data, setData] = useState(undefined);
+
+    const subscribeNewsLetter = async ({ variables }) => {
+        setLoading(true);
+        try {
+            const data = await restClient(`/api/v3/customers/subscribers`, {
+                method: 'POST',
+                headers: {
+                    backendTechnology: 'bigcommerce'
+                },
+                body: JSON.stringify({email: variables.email})
+            });
+
+            setData(subscribeToNewsletterParser(data));
+        } catch (err) {
+            setError(err);
+        }
+        setLoading(false);
+    };
+
+    return { subscribeNewsLetter, loading, data };
 };
 
 export default SubscribeToNewsletter;
