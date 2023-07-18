b2d12c8793e2a383269e795f8675dbf0d698f658
dggorienteed <dgonzalez@orienteed.com>
2023-07-18 12:33:43 +0200

    Update updateCustomerAddressInAddressBook resolver

--- src/resolvers/customer/address/UpdateCustomerAddressInAddressBook/updateCustomerAddressInAddressBook.ts
+++ src/resolvers/customer/address/UpdateCustomerAddressInAddressBook/updateCustomerAddressInAddressBook.ts
@@ -1,11 +1,120 @@
 import { ClientProps } from 'src';
 import { UpdateCustomerAddressInAddressBookMutationVariables } from '@schema';
 
+import { useState } from 'react';
+
 const UpdateCustomerAddressInAddressBook =
     (clientProps: ClientProps) => (resolverProps: UpdateCustomerAddressInAddressBookMutationVariables) => {
-        // Look docs for more info about how to fill this function
+        const { restClient } = clientProps;
+        const [error, setError] = useState(undefined);
+        const [loading, setLoading] = useState(false);
 
-        return { data: {}, loading: false, error: undefined };
+        const updateCustomerAddress = async ({ variables }) => {
+            setLoading(true);
+            try {
+                let region = '';
+
+                if (variables.updated_address) {
+                    if (variables.updated_address.region.region_id) {
+                        const countryData = await restClient(`/api/v2/countries?country_iso2=${variables.updated_address.country_code}`, {
+                            method: 'GET',
+                            headers: {
+                                backendTechnology: ['bigcommerce']
+                            }
+                        });
+
+                        const regionData = await restClient(`/api/v2/countries/${countryData[0].id}/states`, {
+                            method: 'GET',
+                            headers: {
+                                backendTechnology: ['bigcommerce']
+                            }
+                        });
+
+                        if (regionData.find((state) => state.id == variables.updated_address.region.region_id)) {
+                            region = regionData.find((state) => state.id == variables.updated_address.region.region_id).state;
+                        } else {
+                            region = null;
+                        }
+                    } else {
+                        region = variables.updated_address.region.region;
+                    }
+
+                    const rawData = [
+                        {
+                            id: variables.addressId,
+                            country_code: variables.updated_address.country_code,
+                            first_name: variables.updated_address.firstname,
+                            address1: variables.updated_address.street[0],
+                            city: variables.updated_address.city,
+                            state_or_province: region,
+                            postal_code: variables.updated_address.postcode,
+                            phone: variables.updated_address.telephone,
+                            last_name: variables.updated_address.lastname
+                        }
+                    ];
+
+                    await restClient(`/api/v3/customers/addresses`, {
+                        method: 'PUT',
+                        headers: {
+                            backendTechnology: ['bigcommerce']
+                        },
+                        body: JSON.stringify(rawData)
+                    });
+                } else {
+                    if (variables.address.region.region_id) {
+                        const countryData = await restClient(`/api/v2/countries?country_iso2=${variables.address.country_code}`, {
+                            method: 'GET',
+                            headers: {
+                                backendTechnology: ['bigcommerce']
+                            }
+                        });
+
+                        const regionData = await restClient(`/api/v2/countries/${countryData[0].id}/states`, {
+                            method: 'GET',
+                            headers: {
+                                backendTechnology: ['bigcommerce']
+                            }
+                        });
+
+                        if (regionData.find((state) => state.id == variables.address.region.region_id)) {
+                            region = regionData.find((state) => state.id == variables.address.region.region_id).state;
+                        } else {
+                            region = null;
+                        }
+                    } else {
+                        region = variables.address.region.region;
+                    }
+
+                    const rawData = [
+                        {
+                            id: variables.addressId,
+                            country_code: variables.address.country_code,
+                            first_name: variables.address.firstname,
+                            address1: variables.address.street[0],
+                            city: variables.address.city,
+                            state_or_province: region,
+                            postal_code: variables.address.postcode,
+                            phone: variables.address.telephone,
+                            last_name: variables.address.lastname
+                        }
+                    ];
+
+                    await restClient(`/api/v3/customers/addresses`, {
+                        method: 'PUT',
+                        headers: {
+                            backendTechnology: ['bigcommerce']
+                        },
+                        body: JSON.stringify(rawData)
+                    });
+                }
+            } catch (err) {
+                console.log(err);
+                setError(err);
+            }
+            setLoading(false);
+        };
+
+        return { updateCustomerAddress, loading, error };
     };
 
 export default UpdateCustomerAddressInAddressBook;
