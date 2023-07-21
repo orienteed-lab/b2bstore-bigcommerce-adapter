c2e679544147853da5f4a06688e094f87be3bb3e
dggorienteed <dgonzalez@orienteed.com>
2023-07-21 09:15:40 +0200

    Fix getCustomerInformation resolver

--- src/resolvers/customer/GetCustomerInformation/getCustomerInformation.ts
+++ src/resolvers/customer/GetCustomerInformation/getCustomerInformation.ts
@@ -1,61 +1,61 @@
 import { ClientProps } from 'src';
-import { GetCustomerInformationQueryVariables } from '@schema';
 import { getCustomerInformationParser } from './getCustomerInformationParser';
 import DEFAULT_OPERATIONS from './getCustomerInformation.gql';
 import { useCallback, useEffect, useState } from 'react';
-import { parse } from 'graphql';
 
 interface GetCustomerInformationProps {
-    vars: { lazy: boolean; isSignedIn: boolean };
+    isAwait?: boolean;
+    isSignedIn?: boolean;
 }
-const GetCustomerInformation = (clientProps: ClientProps) => (resolverProps: GetCustomerInformationProps = {vars: {lazy: false, isSignedIn: true}}) => {
-    const { useAwaitQuery, mergeOperations, restClient } = clientProps;
+const GetCustomerInformation =
+    (clientProps: ClientProps) =>
+    (resolverProps: GetCustomerInformationProps = { isAwait: false, isSignedIn: true }) => {
+        const { useAwaitQuery, mergeOperations, restClient } = clientProps;
+        const { isAwait, isSignedIn } = resolverProps;
 
-    const operations = mergeOperations(DEFAULT_OPERATIONS);
-    const { getCustomerInformationQuery } = operations;
-    const { vars } = resolverProps;
-    const [loading, setLoading] = useState(false);
-    const [data, setData] = useState();
-    const [error, setError] = useState();
-    const performQuery = useAwaitQuery(getCustomerInformationQuery);
+        const operations = mergeOperations(DEFAULT_OPERATIONS);
+        const { getCustomerInformationQuery } = operations;
 
-    const customerInformation = useCallback(async () => {
-       if(vars.isSignedIn){
-        setLoading(true);
-        const { data: dataCustomer } = await performQuery({
-            context: {
-                headers: {
-                    backendTechnology: ['bigcommerce']
+        const [loading, setLoading] = useState(false);
+        const [data, setData] = useState(undefined);
+        const [error, setError] = useState(undefined);
+        const performQuery = useAwaitQuery(getCustomerInformationQuery);
+
+        const getCustomerInformation = useCallback(async () => {
+            setLoading(true);
+            try {
+                if (isSignedIn) {
+                    const { data: dataCustomer } = await performQuery({
+                        context: {
+                            headers: {
+                                backendTechnology: ['bigcommerce']
+                            }
+                        }
+                    });
+
+                    const { data: rawData } = await restClient(`/api/v3/customers/subscribers/?email:in=${dataCustomer.customer.email}`, {
+                        method: 'GET',
+                        headers: {
+                            backendTechnology: 'bigcommerce'
+                        }
+                    });
+
+                    setData(getCustomerInformationParser(dataCustomer, rawData));
                 }
+            } catch (err) {
+                setError(err);
             }
-        });
+            setLoading(false);
+            return { data };
+        }, []);
 
-        let parsedData = undefined;
-        if (dataCustomer) {
-            const { data: rawData } = await restClient(`/api/v3/customers/subscribers/?email:in=${dataCustomer.customer.email}`, {
-                method: 'GET',
-                headers: {
-                    backendTechnology: 'bigcommerce'
-                }
-            });
-            if (rawData) {
-                parsedData = getCustomerInformationParser(dataCustomer, rawData);
-                setLoading(false);
-                setData(parsedData);
+        useEffect(() => {
+            if (!isAwait) {
+                getCustomerInformation();
             }
-        }
-        setError(error);
-        return { data };
-    }
-    }, [performQuery, restClient, getCustomerInformationParser]);
+        }, []);
 
-    useEffect(() => {
-        if (!vars.lazy) {
-            customerInformation();
-        }
-    }, [vars, customerInformation]);
-
-    return { loading, error, data, customerInformation };
-};
+        return { loading, error, data, getCustomerInformation };
+    };
 
 export default GetCustomerInformation;
