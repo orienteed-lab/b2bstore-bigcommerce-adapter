b6659e01d5ad4c6ac7d5086e773c41cff06026e0
dggorienteed <dgonzalez@orienteed.com>
2023-08-11 13:45:50 +0200

    Update updateConfigurableOptions resolver

--- src/resolvers/product/UpdateConfigurableOptions/updateConfigurableOptions.gql.ts
+++ src/resolvers/product/UpdateConfigurableOptions/updateConfigurableOptions.gql.ts
@@ -1,9 +1,36 @@
 import { gql } from '@apollo/client';
 
 export const UPDATE_CONFIGURABLE_OPTIONS = gql`
-    # Your query here
+    mutation updateConfigurableOptions($item: UpdateCartLineItemInput!) {
+        cart {
+            updateCartLineItem(input: $item){
+                cart {
+                    entityId
+                }
+            }
+        }
+    }
+`;
+
+export const GET_PRODUCT_ID_BY_SKU = gql`
+    query getProductIdBySku($parentSku: String!) {
+        site {
+            product(sku: $parentSku) {
+                entityId
+                variants {
+                    edges {
+                        node {
+                            entityId
+                            sku
+                        }
+                    }
+                }
+            }
+        }
+    }
 `;
 
 export default {
-    updateConfigurableOptionsMutation: UPDATE_CONFIGURABLE_OPTIONS
+    updateConfigurableOptionsMutation: UPDATE_CONFIGURABLE_OPTIONS,
+    getProductIdBySkuQuery: GET_PRODUCT_ID_BY_SKU
 };
--- src/resolvers/product/UpdateConfigurableOptions/updateConfigurableOptions.ts
+++ src/resolvers/product/UpdateConfigurableOptions/updateConfigurableOptions.ts
@@ -1,10 +1,63 @@
 import { ClientProps } from 'src';
 import { UpdateConfigurableOptionsMutationVariables } from '@schema';
 
-const UpdateConfigurableOptions = (clientProps: ClientProps) => (resolverProps: UpdateConfigurableOptionsMutationVariables) => {
-    // Look docs for more info about how to fill this function
+import DEFAULT_OPERATIONS from './updateConfigurableOptions.gql';
+import { useState } from 'react';
 
-    return { data: {}, loading: false, error: undefined };
+const UpdateConfigurableOptions = (clientProps: ClientProps) => (resolverProps: UpdateConfigurableOptionsMutationVariables) => {
+    const { mergeOperations, useAwaitQuery, useMutation } = clientProps;
+    const [called, setCalled] = useState(false);
+    const [error, setError] = useState(undefined);
+    const [loading, setLoading] = useState(false);
+
+    const { updateConfigurableOptionsMutation, getProductIdBySkuQuery } = mergeOperations(DEFAULT_OPERATIONS);
+    const getProductId = useAwaitQuery(getProductIdBySkuQuery);
+    const [updateOptions] = useMutation(updateConfigurableOptionsMutation);
+
+    const updateConfigurableOptions = async ({ variables }) => {
+        setCalled(true);
+        setLoading(true);
+        try {
+            const { data: prodData } = await getProductId({
+                context: {
+                    headers: {
+                        backendTechnology: ['bigcommerce']
+                    }
+                },
+                variables: { parentSku: variables.parentSku }
+            });
+
+            const productId = prodData.site.product.entityId;
+            const variantId = prodData.site.product.variants.edges.find((variant) => variant.node.sku === variables.variantSku).node
+                .entityId;
+
+            await updateOptions({
+                context: {
+                    headers: {
+                        backendTechnology: ['bigcommerce']
+                    }
+                },
+                variables: {
+                    item: {
+                        cartEntityId: variables.cartId,
+                        lineItemEntityId: variables.cartItemId,
+                        data: {
+                            lineItem: {
+                                quantity: variables.quantity,
+                                productEntityId: productId,
+                                variantEntityId: variantId
+                            }
+                        }
+                    }
+                }
+            });
+        } catch (err) {
+            setError(err);
+        }
+        setLoading(false);
+    };
+
+    return { updateConfigurableOptions, loading, error, called };
 };
 
 export default UpdateConfigurableOptions;
