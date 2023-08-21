7c05b0eca75d8b3651263fff2c1426012b3e5f93
dggorienteed <dgonzalez@orienteed.com>
2023-08-21 10:43:45 +0200

    Fix getCustomerSubscription resolver

--- src/resolvers/customer/newsletter/GetCustomerSubscription/getCustomerSubscriptionParser.ts
+++ src/resolvers/customer/newsletter/GetCustomerSubscription/getCustomerSubscriptionParser.ts
@@ -3,7 +3,7 @@ import { GetCustomerSubscriptionQuery } from '@schema';
 export const getCustomerSubscriptionParser = ({data}: any): GetCustomerSubscriptionQuery => {
     return {
         customer: {
-            is_subscribed: data.length !== 0 ? true : false
+            is_subscribed: data.length !== 0
         }
     };
 };
