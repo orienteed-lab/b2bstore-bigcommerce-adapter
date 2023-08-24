1f4077957ce2a1b47b297e84144dedc8e9c5dc9c
David Gutierrez <dgutierrez@orienteed.com>
2023-08-24 13:43:31 +0200

    Fix getImageBySkuResolver

--- src/resolvers/product/GetImageBySku/getImageBySku.ts
+++ src/resolvers/product/GetImageBySku/getImageBySku.ts
@@ -6,33 +6,36 @@ import DEFAULT_OPERATIONS from './getImageBySku.gql'
 
 const GetImageBySku = (clientProps: ClientProps) => (resolverProps: GetImageBySkuQueryVariables) => {
     const { mergeOperations, useAwaitQuery } = clientProps;
-    const { sku } = resolverProps;
 
     const operations = mergeOperations(DEFAULT_OPERATIONS);
     const { getImageBySkuQuery } = operations;
 
-    const { data, loading, error } = useAwaitQuery(getImageBySkuQuery,{
-        context: {
-            headers: {
-                backendTechnology: ['bigcommerce']
+    const getImage = useAwaitQuery(getImageBySkuQuery);
+
+    const fetchProductImage = async ({variables}) => {
+        let parsedData = undefined;
+
+        const { data } = await getImage({
+            context: {
+                headers: {
+                    backendTechnology: ['bigcommerce']
+                }
+            },
+            variables: {
+                productSku: variables.sku
             }
-        },
-        variables: {
-            productSku: sku
-        }
+        });
 
-    });
-
-    let parsedData = {};
-    if (data) {
         try {
             parsedData = getImageBySkuParser(data);
         } catch (e) {
             console.error(e);
         }
+
+        return { data: parsedData }
     }
 
-    return { data: parsedData, loading, error };
+    return { fetchProductImage };
 };
 
 export default GetImageBySku;
