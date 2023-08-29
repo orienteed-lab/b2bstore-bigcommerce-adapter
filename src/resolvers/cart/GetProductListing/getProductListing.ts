efd1be3ec5351ebe860ce63270bc17eed0fb565f
dggorienteed <dgonzalez@orienteed.com>
2023-08-29 14:43:16 +0200

    Fix getProductListing resolver

--- src/resolvers/cart/GetProductListing/getProductListing.ts
+++ src/resolvers/cart/GetProductListing/getProductListing.ts
@@ -2,7 +2,7 @@ import { ClientProps } from 'src';
 import { GetProductListingQueryVariables } from '@schema';
 
 import { getProductListingParser } from './getProductListingParser';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 interface GetCartDetailsProps {
     variables: GetProductListingQueryVariables;
@@ -10,55 +10,34 @@ interface GetCartDetailsProps {
 
 const GetProductListing = (clientProps: ClientProps) => (resolverProps: GetCartDetailsProps) => {
     const { restClient } = clientProps;
-    const {
-        variables: { cartId }
-    } = resolverProps;
-
-
-    console.log({ cartId })
 
     const [loading, setLoading] = useState(true);
     const [data, setData] = useState<any>(null);
     const [error, setError] = useState(null);
+    const [called, setCalled] = useState(false);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            setLoading(true);
-            try {
-                const rawData = await restClient(
-                    `/api/v3/carts/${cartId}?include=line_items.physical_items.options,line_items.digital_items.options`,
-                    {
-                        method: 'GET',
-                        headers: {
-                            backendTechnology: 'bigcommerce'
-                        }
+    const fetchProductListing = async ({variables}) => {
+        setLoading(true);
+        setCalled(true);
+        try {
+            const rawData = await restClient(
+                `/api/v3/carts/${variables.cartId}?include=line_items.physical_items.options,line_items.digital_items.options`,
+                {
+                    method: 'GET',
+                    headers: {
+                        backendTechnology: 'bigcommerce'
                     }
-                );
-                // setData(generateTokenParser(rawData));
-                setData(rawData);
-            } catch (err: any) {
-                setError(err);
-            }
-            setLoading(false);
-        };
-        fetchData();
-    }, []);
+                }
+            );
 
-    console.log('antes del parser', data);
+            setData(getProductListingParser(rawData));
+        } catch (err: any) {
+            setError(err);
+        }
+        setLoading(false);
+    };
 
-    let parsedData = undefined;
-    if (data) {
-        // try {
-        parsedData = getProductListingParser(data);
-        // } catch (e) {
-        //     console.error(e);
-        // }
-    }
-
-    console.log('despues del parser', parsedData);
-
-    //return { data, loading, error };
-    return { data: parsedData, loading, error };
+    return { fetchProductListing, data, loading, error, called };
 };
 
 export default GetProductListing;
