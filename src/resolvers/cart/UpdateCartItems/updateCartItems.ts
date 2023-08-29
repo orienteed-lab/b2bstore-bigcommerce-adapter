68f2d534585bda278329830c4b66d909a3a6be43
dggorienteed <dgonzalez@orienteed.com>
2023-08-29 13:38:22 +0200

    Update updateCartItems resolver

--- src/resolvers/cart/UpdateCartItems/updateCartItems.ts
+++ src/resolvers/cart/UpdateCartItems/updateCartItems.ts
@@ -1,10 +1,57 @@
 import { ClientProps } from 'src';
 import { UpdateCartItemsMutationVariables } from '@schema';
 
-const UpdateCartItems = (clientProps: ClientProps) => (resolverProps: UpdateCartItemsMutationVariables) => {
-    // Look docs for more info about how to fill this function
+import { useState } from 'react';
 
-    return { data: {}, loading: false, error: undefined };
+const UpdateCartItems = (clientProps: ClientProps) => (resolverProps: UpdateCartItemsMutationVariables) => {
+    const { restClient } = clientProps;
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(undefined);
+    const [called, setCalled] = useState(false);
+
+    const updateItemQuantity = async ({ variables }) => {
+        setLoading(true);
+        setCalled(true);
+        try {
+            let itemEntityId = undefined;
+            const rawData = await restClient(`/api/v3/carts/${variables.cartId}`, {
+                method: 'GET',
+                headers: {
+                    backendTechnology: 'bigcommerce'
+                }
+            });
+
+            if (
+                rawData.data.line_items.physical_items &&
+                rawData.data.line_items.physical_items?.find((item) => item.id === variables.itemId)
+            ) {
+                itemEntityId = rawData.data.line_items.physical_items?.find((item) => item.id === variables.itemId).product_id;
+            } else if (
+                rawData.data.line_items.digital_items &&
+                rawData.data.line_items.digital_items?.find((item) => item.id === variables.itemId)
+            ) {
+                itemEntityId = rawData.data.line_items.digital_items?.find((item) => item.id === variables.itemId).product_id;
+            }
+
+            await restClient(`/api/v3/carts/${variables.cartId}/items/${variables.itemId}`, {
+                method: 'PUT',
+                headers: {
+                    backendTechnology: 'bigcommerce'
+                },
+                body: JSON.stringify({
+                    line_item: {
+                        quantity: variables.quantity,
+                        product_id: itemEntityId
+                    }
+                })
+            });
+        } catch (err) {
+            setError(err);
+        }
+        setLoading(false);
+    };
+
+    return { updateItemQuantity, loading, error, called };
 };
 
 export default UpdateCartItems;
