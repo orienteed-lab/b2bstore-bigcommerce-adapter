64c17f835a7d38b9a7a8db01e86abfae4ba8d434
dggorienteed <dgonzalez@orienteed.com>
2023-09-29 12:44:14 +0200

    Fix hook error in getAvailableSortMethodsByCategory resolver

--- src/resolvers/product/GetAvailableSortMethodsByCategory/getAvailableSortMethodsByCategory.ts
+++ src/resolvers/product/GetAvailableSortMethodsByCategory/getAvailableSortMethodsByCategory.ts
@@ -2,43 +2,25 @@ import { ClientProps } from 'src';
 import { GetAvailableSortMethodsByCategoryQueryVariables } from '@schema';
 
 import { getAvailableSortMethodsByCategoryParser } from './getAvailableSortMethodsByCategoryParser';
-import { useEffect, useState } from 'react';
+import { useCallback, useState } from 'react';
 
 const GetAvailableSortMethodsByCategory =
     (clientProps: ClientProps) => (resolverProps: GetAvailableSortMethodsByCategoryQueryVariables) => {
         const { restClient } = clientProps;
-         //const { categoryIdFilter }  = resolverProps;
+        const [data, setData] = useState<any>(undefined);
 
-        const [loading, setLoading] = useState(true);
-        const [data, setData] = useState<any>(null);
-        const [error, setError] = useState(null);
+        const getSortMethods = useCallback(async ({variables}) => {
+            const rawData = await restClient(`/api/v3/catalog/categories/${variables.categoryIdFilter.in}/products/sort-order`, {
+                method: 'GET',
+                headers: {
+                    backendTechnology: 'bigcommerce'
+                }
+            });
 
-        const getSortMethods = async (categoryIdFilter) => {
-            setLoading(true);
-            try {
-                const rawData = await restClient(`/api/v3/catalog/categories/${categoryIdFilter}/products/sort-order`, {
-                    method: 'GET',
-                    headers: {
-                        backendTechnology: 'bigcommerce'
-                    }
-                });
+            setData(getAvailableSortMethodsByCategoryParser(rawData));
+        }, []);
 
-                setData(rawData);
-            } catch (err: any) {
-                setError(err);
-            }
-            setLoading(false);
-
-        };
-
-
-
-        let parsedData = undefined;
-        if (data) {
-            parsedData = getAvailableSortMethodsByCategoryParser(data);
-        }
-        return { data: parsedData, loading, error, getSortMethods };
-
+        return { data, getSortMethods };
     };
 
 export default GetAvailableSortMethodsByCategory;
