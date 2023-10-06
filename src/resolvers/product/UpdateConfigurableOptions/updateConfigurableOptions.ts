85b668b7c39726225f7dee9bc929de3541d07608
dggorienteed <dgonzalez@orienteed.com>
2023-10-06 13:00:52 +0200

    Fix operation in updateConfigurableOptions resolver

--- src/resolvers/product/UpdateConfigurableOptions/updateConfigurableOptions.ts
+++ src/resolvers/product/UpdateConfigurableOptions/updateConfigurableOptions.ts
@@ -5,7 +5,7 @@ import DEFAULT_OPERATIONS from './updateConfigurableOptions.gql';
 import { useState } from 'react';
 
 const UpdateConfigurableOptions = (clientProps: ClientProps) => (resolverProps: UpdateConfigurableOptionsMutationVariables) => {
-    const { mergeOperations, useAwaitQuery, useMutation } = clientProps;
+    const { mergeOperations, useAwaitQuery, useMutation, restClient } = clientProps;
     const [called, setCalled] = useState(false);
     const [error, setError] = useState(undefined);
     const [loading, setLoading] = useState(false);
@@ -31,26 +31,55 @@ const UpdateConfigurableOptions = (clientProps: ClientProps) => (resolverProps: 
             const variantId = prodData.site.product.variants.edges.find((variant) => variant.node.sku === variables.variantSku).node
                 .entityId;
 
-            await updateOptions({
-                context: {
+            // await updateOptions({
+            //     context: {
+            //         headers: {
+            //             backendTechnology: ['bigcommerce']
+            //         }
+            //     },
+            //     variables: {
+            //         item: {
+            //             cartEntityId: variables.cartId,
+            //             lineItemEntityId: variables.cartItemId,
+            //             data: {
+            //                 lineItem: {
+            //                     quantity: variables.quantity,
+            //                     productEntityId: productId,
+            //                     variantEntityId: variantId
+            //                 }
+            //             }
+            //         }
+            //     }
+            // });
+
+            await restClient(
+                `/api/v3/carts/${variables.cartId}/items/${variables.cartItemId}`,
+                {
+                    method: 'DELETE',
                     headers: {
-                        backendTechnology: ['bigcommerce']
-                    }
-                },
-                variables: {
-                    item: {
-                        cartEntityId: variables.cartId,
-                        lineItemEntityId: variables.cartItemId,
-                        data: {
-                            lineItem: {
-                                quantity: variables.quantity,
-                                productEntityId: productId,
-                                variantEntityId: variantId
-                            }
-                        }
+                        backendTechnology: 'bigcommerce'
                     }
                 }
-            });
+            );
+
+            const body = {
+                line_items: [{
+                    quantity: variables.quantity,
+                    product_id: productId,
+                    variant_id: variantId
+                }]
+            };
+
+            await restClient(
+                `/api/v3/carts/${variables.cartId}/items`,
+                {
+                    method: 'POST',
+                    headers: {
+                        backendTechnology: 'bigcommerce'
+                    },
+                    body: JSON.stringify(body)
+                }
+            );
         } catch (err) {
             setError(err);
         }
