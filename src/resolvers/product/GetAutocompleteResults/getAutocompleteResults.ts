ee030981c69b7f4c780e0ab5b72beb67b1d653fd
dggorienteed <dgonzalez@orienteed.com>
2023-10-09 16:48:21 +0200

    Fix data setting in getAutocompleteResults resolver

--- src/resolvers/product/GetAutocompleteResults/getAutocompleteResults.ts
+++ src/resolvers/product/GetAutocompleteResults/getAutocompleteResults.ts
@@ -3,32 +3,47 @@ import { GetAutocompleteResultsQueryVariables } from '@schema';
 
 import { getAutocompleteResultsParser } from './getAutocompleteResultsParser';
 import DEFAULT_OPERATIONS from './getAutocompleteResults.gql';
+import { useState } from 'react';
 
-const GetAutocompleteResults = (clientProps: ClientProps) => (resolverProps: GetAutocompleteResultsQueryVariables) => {
-    const { mergeOperations, useLazyQuery } = clientProps;
+interface GetAutocompleteResultsProps extends GetAutocompleteResultsQueryVariables {
+    hasVars: boolean
+}
+
+const GetAutocompleteResults = (clientProps: ClientProps) => (resolverProps: GetAutocompleteResultsProps = {inputText: null, hasVars: false}) => {
+    const { mergeOperations, useAwaitQuery } = clientProps;
+    const { inputText } = resolverProps;
+    const [data, setData] = useState(undefined);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(undefined);
 
     const operations = mergeOperations(DEFAULT_OPERATIONS);
     const { getAutocompleteResultsQuery } = operations;
+    const getResults = useAwaitQuery(getAutocompleteResultsQuery);
 
-    const [runSearch, productResult] = useLazyQuery(getAutocompleteResultsQuery, {
-        fetchPolicy: 'cache-and-network',
-        nextFetchPolicy: 'cache-first',
-        context: {
-            headers: {
-                backendTechnology: ['bigcommerce']
-            }
-        }
-    });
+    const runSearch = async ({variables}) => {
+        setLoading(true);
+        try {
+            const { data: resultData } = await getResults({
+                context: {
+                    headers: {
+                        backendTechnology: ['bigcommerce']
+                    }
+                },
+                variables: {
+                    search: {
+                        searchTerm: variables.inputText || inputText
+                    }
+                }
+            });
 
-    const { data } = productResult;
-
-    let parsedData = undefined;
-
-    if (data) {
-        parsedData = getAutocompleteResultsParser(data);
+            setData(getAutocompleteResultsParser(resultData));
+        } catch (err) {
+            setError(err);
+        };
+        setLoading(true);
     }
 
-    return { productResult: { data: parsedData }, runSearch };
+    return { productResult: { data, loading, error }, runSearch };
 };
 
 export default GetAutocompleteResults;
