cc18142c684e3579fbc18c716f4bc9a204803c11
dggorienteed <dgonzalez@orienteed.com>
2023-10-11 08:57:47 +0200

    Update applyCouponToCart resolver

--- src/resolvers/cart/coupon/ApplyCouponToCart/applyCouponToCart.ts
+++ src/resolvers/cart/coupon/ApplyCouponToCart/applyCouponToCart.ts
@@ -1,10 +1,33 @@
 import { ClientProps } from 'src';
 import { ApplyCouponToCartMutationVariables } from '@schema';
+import { useState } from 'react';
 
 const ApplyCouponToCart = (clientProps: ClientProps) => (resolverProps: ApplyCouponToCartMutationVariables) => {
-    // Look docs for more info about how to fill this function
+    const { restClient } = clientProps;
+    const [called, setCalled] = useState(false);
+    const [error, setError] = useState(undefined);
+    const [loading, setLoading] = useState(false);
 
-    return { data: {}, loading: false, error: undefined };
+    const applyCoupon = async ({ variables }) => {
+        setLoading(true);
+        setCalled(true);
+        try {
+            const coupon = { coupon_code: variables.couponCode };
+
+            await restClient(`/api/v3/checkouts/${variables.cartId}/coupons`, {
+                method: 'POST',
+                headers: {
+                    backendTechnology: 'bigcommerce'
+                },
+                body: JSON.stringify(coupon)
+            });
+        } catch (err) {
+            setError(err);
+        }
+        setLoading(false);
+    };
+
+    return { applyCoupon, called, loading, error };
 };
 
 export default ApplyCouponToCart;
