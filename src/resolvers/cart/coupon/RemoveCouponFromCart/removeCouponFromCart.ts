ce873ea6638008bae1d4902c922bbb4b6ec34f51
dggorienteed <dgonzalez@orienteed.com>
2023-10-11 08:58:44 +0200

    Update removeCouponFromCart resolver

--- src/resolvers/cart/coupon/RemoveCouponFromCart/removeCouponFromCart.ts
+++ src/resolvers/cart/coupon/RemoveCouponFromCart/removeCouponFromCart.ts
@@ -1,10 +1,30 @@
 import { ClientProps } from 'src';
 import { RemoveCouponFromCartMutationVariables } from '@schema';
+import { useState } from 'react';
 
 const RemoveCouponFromCart = (clientProps: ClientProps) => (resolverProps: RemoveCouponFromCartMutationVariables) => {
-    // Look docs for more info about how to fill this function
+    const { restClient } = clientProps;
+    const [called, setCalled] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(undefined);
 
-    return { data: {}, loading: false, error: undefined };
+    const removeCoupon = async ({ variables }) => {
+        setLoading(true);
+        setCalled(true);
+        try {
+            await restClient(`/api/v3/checkouts/${variables.cartId}/coupons/${variables.couponCode}`, {
+                method: 'DELETE',
+                headers: {
+                    backendTechnology: 'bigcommerce'
+                }
+            });
+        } catch (err) {
+            setError(err);
+        }
+        setLoading(false);
+    };
+
+    return { removeCoupon, called, loading, error };
 };
 
 export default RemoveCouponFromCart;
