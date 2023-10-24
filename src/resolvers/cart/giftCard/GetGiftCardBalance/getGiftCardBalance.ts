0876ec578afc983c0761afec774dd53bceada93b
dggorienteed <dgonzalez@orienteed.com>
2023-10-24 13:45:31 +0200

    Update getGiftCardBalance resolver

--- src/resolvers/cart/giftCard/GetGiftCardBalance/getGiftCardBalance.ts
+++ src/resolvers/cart/giftCard/GetGiftCardBalance/getGiftCardBalance.ts
@@ -1,10 +1,32 @@
 import { ClientProps } from 'src';
 import { GetGiftCardBalanceQueryVariables } from '@schema';
+import { useState } from 'react';
+
+import { getGiftCardBalanceParser } from './getGiftCardBalanceParser';
 
 const GetGiftCardBalance = (clientProps: ClientProps) => (resolverProps: GetGiftCardBalanceQueryVariables) => {
-    // Look docs for more info about how to fill this function
+    const { restClient } = clientProps;
+    const [data, setData] = useState(undefined);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(undefined);
 
-    return { data: {}, loading: false, error: undefined };
+    const checkCardBalance = async ({variables}) => {
+        setLoading(true);
+        try {
+            const response = await restClient(`/api/v2/gift_certificates?code=${variables.giftCardCode}`, {
+                method: 'GET',
+                headers: {
+                    backendTechnology: 'bigcommerce'
+                }
+            });
+            setData(getGiftCardBalanceParser(response[0]));
+        } catch (err) {
+            setError(err);
+        }
+        setLoading(false);
+    }
+
+    return { balanceResult: {data, loading, error}, checkCardBalance };
 };
 
 export default GetGiftCardBalance;
--- src/resolvers/cart/giftCard/GetGiftCardBalance/getGiftCardBalanceParser.ts
+++ src/resolvers/cart/giftCard/GetGiftCardBalance/getGiftCardBalanceParser.ts
@@ -1,6 +1,22 @@
 import { GetGiftCardBalanceQuery } from '@schema';
 
 export const getGiftCardBalanceParser = (data: any): GetGiftCardBalanceQuery => {
-    // Your parser logic here
-    return data;
+    const getDate = (bcDate) => {
+        let d = new Date(0); // The 0 there is the key, which sets the date to the epoch
+        d.setUTCSeconds(bcDate);
+
+        return `${d.getFullYear()}-${d.getMonth()}-${d.getDay()} ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
+    };
+
+    return {
+        giftCardAccount: {
+            balance: {
+                currency: data.currency_code,
+                value: data.balance
+            },
+            code: data.code,
+            expiration_date: data.expiry_date !== '0' ? getDate(data.expiry_date) : data.expiry_date,
+            id: data.code
+        }
+    };
 };
