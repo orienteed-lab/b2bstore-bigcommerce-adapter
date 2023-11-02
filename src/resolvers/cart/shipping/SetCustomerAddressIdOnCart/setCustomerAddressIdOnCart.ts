50641b32f8b4ebb3e24c1c15502911de3ffb4362
dggorienteed <dgonzalez@orienteed.com>
2023-11-02 09:13:39 +0100

    Update setCustomerAddressIdOnCart resolver

--- src/resolvers/cart/shipping/SetCustomerAddressIdOnCart/setCustomerAddressIdOnCart.ts
+++ src/resolvers/cart/shipping/SetCustomerAddressIdOnCart/setCustomerAddressIdOnCart.ts
@@ -1,10 +1,67 @@
 import { ClientProps } from 'src';
 import { SetCustomerAddressIdOnCartMutationVariables } from '@schema';
 
-const SetCustomerAddressIdOnCart = (clientProps: ClientProps) => (resolverProps: SetCustomerAddressIdOnCartMutationVariables) => {
-    // Look docs for more info about how to fill this function
+import { setCustomerAddressIdOnCartParser } from './setCustomerAddressIdOnCartParser';
+import { useState } from 'react';
 
-    return { data: {}, loading: false, error: undefined };
+interface SetCustomerAddressIdOnCartProps extends SetCustomerAddressIdOnCartMutationVariables {
+    onSuccess?: any,
+    hasOnSuccess: boolean
+}
+
+const SetCustomerAddressIdOnCart = (clientProps: ClientProps) => (resolverProps: SetCustomerAddressIdOnCartProps) => {
+    const { restClient } = clientProps;
+    const { hasOnSuccess } = resolverProps;
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(undefined);
+
+    const setDefaultAddressOnCart = async ({ variables }) => {
+        setLoading(true);
+        try {
+            const { data: addressData } = await restClient(`/api/v3/customers/addresses?id:in=${variables.addressId}`, {
+                method: 'GET',
+                headers: {
+                    backendTechnology: 'bigcommerce'
+                }
+            });
+
+            const { data: checkoutData } = await restClient(`/api/v3/checkouts/${variables.cartId}`, {
+                method: 'GET',
+                headers: {
+                    backendTechnology: 'bigcommerce'
+                }
+            });
+
+            let body = setCustomerAddressIdOnCartParser(addressData[0], checkoutData);
+
+            if (checkoutData.consignments.length !== 0) {
+                await restClient(`/api/v3/checkouts/${variables.cartId}/consignments/${checkoutData.consignments[0].id}`, {
+                    method: 'PUT',
+                    headers: {
+                        backendTechnology: 'bigcommerce'
+                    },
+                    body: JSON.stringify(body[0])
+                });
+            } else {
+                await restClient(`/api/v3/checkouts/${variables.cartId}/consignments`, {
+                    method: 'POST',
+                    headers: {
+                        backendTechnology: 'bigcommerce'
+                    },
+                    body: JSON.stringify(body)
+                });
+            }
+        } catch (err) {
+            setError(err);
+        }
+        setLoading(false);
+        if (hasOnSuccess && !error) {
+            const { onSuccess } = resolverProps;
+            onSuccess();
+        }
+    };
+
+    return { setCustomerAddressOnCart: setDefaultAddressOnCart, setDefaultAddressOnCart, loading, error };
 };
 
 export default SetCustomerAddressIdOnCart;
