167d3b71a778c91d21900fd0a123771e9d03d0f5
dggorienteed <dgonzalez@orienteed.com>
2023-11-02 09:11:50 +0100

    Update getDefaultShipping resolver

--- src/resolvers/cart/shipping/GetDefaultShipping/getDefaultShipping.ts
+++ src/resolvers/cart/shipping/GetDefaultShipping/getDefaultShipping.ts
@@ -1,9 +1,53 @@
+import { useEffect, useState } from 'react';
 import { ClientProps } from 'src';
 
-const GetDefaultShipping = (clientProps: ClientProps) => () => {
-    // Look docs for more info about how to fill this function
+import DEFAULT_OPERATIONS from './getDefaultShipping.gql';
+import { getDefaultShippingParser } from './getDefaultShippingParser';
 
-    return { data: {}, loading: false, error: undefined };
-};
+interface GetDefaultShippingProps {
+    isSignedIn?: boolean;
+}
+
+const GetDefaultShipping =
+    (clientProps: ClientProps) =>
+    (resolverProps: GetDefaultShippingProps = { isSignedIn: false }) => {
+        const { mergeOperations, restClient, useAwaitQuery } = clientProps;
+        const { isSignedIn } = resolverProps;
+        const [data, setData] = useState(undefined);
+        const [loading, setLoading] = useState(true);
+
+        const { getCustomerIdQuery } = mergeOperations(DEFAULT_OPERATIONS);
+        const getCustomer = useAwaitQuery(getCustomerIdQuery);
+
+        useEffect(() => {
+            const fetchData = async () => {
+                setLoading(true);
+                if (isSignedIn) {
+                    const { data: customerData } = await getCustomer({
+                        context: {
+                            headers: {
+                                backendTechnology: ['bigcommerce']
+                            }
+                        }
+                    });
+
+                    const {data: rawData} = await restClient(
+                        `/api/v3/customers/addresses?customer_id:in=${customerData.customer.entityId}`,
+                        {
+                            method: 'GET',
+                            headers: {
+                                backendTechnology: 'bigcommerce'
+                            }
+                        }
+                    );
+                    setData(getDefaultShippingParser(rawData));
+                }
+                setLoading(false);
+            };
+            fetchData();
+        }, []);
+
+        return { data, loading };
+    };
 
 export default GetDefaultShipping;
--- src/resolvers/cart/shipping/GetDefaultShipping/getDefaultShippingParser.ts
+++ src/resolvers/cart/shipping/GetDefaultShipping/getDefaultShippingParser.ts
@@ -1,6 +1,9 @@
 import { GetDefaultShippingQuery } from '@schema';
 
 export const getDefaultShippingParser = (data: any): GetDefaultShippingQuery => {
-    // Your parser logic here
-    return data;
+    return data.length ? {
+        customer: {
+            default_shipping: data[0].id.toString()
+        }
+    } : null
 };
