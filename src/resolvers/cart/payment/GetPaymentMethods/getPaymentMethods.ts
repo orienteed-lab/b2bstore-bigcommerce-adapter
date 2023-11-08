13d6644608ea5a7de6df4a83d0332ff912ff10f5
dggorienteed <dgonzalez@orienteed.com>
2023-11-08 13:19:57 +0100

    Update getPaymentMethods resolver

--- src/resolvers/cart/payment/GetPaymentMethods/getPaymentMethods.ts
+++ src/resolvers/cart/payment/GetPaymentMethods/getPaymentMethods.ts
@@ -1,10 +1,134 @@
 import { ClientProps } from 'src';
 import { GetPaymentMethodsQueryVariables } from '@schema';
+import { useEffect, useState } from 'react';
+
+import { getPaymentMethodsParser } from './getPaymentMethodsParser';
 
 const GetPaymentMethods = (clientProps: ClientProps) => (resolverProps: GetPaymentMethodsQueryVariables) => {
-    // Look docs for more info about how to fill this function
+    /*
+        1. Check if there is and order for that checkout
+        YES:
+            2. Retrieve that order's payment options
+            3. Retrieve the selected payment method if has one, if not the first in the available list is selected
+        NO:
+            2. Try to create an order. Success?
+            YES:
+                3. Get payment methods from that order
+                4. Retrieve the selected payment method if has one, if not the first in the available list is selected
+            NO:
+                3. Update billing address
+                4. Update shipping_option_id in consignment
+                5. Create an order from the checkout
+                6. Get payment methods from that order
+                7. Retrieve the selected payment method if has one, if not the first in the available list is selected
+    */
+    const { restClient } = clientProps;
+    const { cartId } = resolverProps;
+    const [data, setData] = useState(undefined);
+    const [loading, setLoading] = useState(true);
 
-    return { data: {}, loading: false, error: undefined };
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            if (cartId) {
+                try {
+                    const orderData = await restClient(`/api/v2/orders?cart_id=${cartId}`, {
+                        method: 'GET',
+                        headers: {
+                            backendTechnology: 'bigcommerce'
+                        }
+                    });
+
+                    const { data: paymentOptions } = await restClient(`/api/v3/payments/methods?order_id=${orderData[0].id}`, {
+                        method: 'GET',
+                        headers: {
+                            backendTechnology: 'bigcommerce'
+                        }
+                    });
+
+                    setData(getPaymentMethodsParser(paymentOptions, orderData));
+                } catch (e) {
+                    try {
+                        const { data: order } = await restClient(`/api/v3/checkouts/${cartId}/orders`, {
+                            method: 'POST',
+                            headers: {
+                                backendTechnology: 'bigcommerce'
+                            }
+                        });
+
+                        const { data: paymentOptions } = await restClient(`/api/v3/payments/methods?order_id=${order.id}`, {
+                            method: 'GET',
+                            headers: {
+                                backendTechnology: 'bigcommerce'
+                            }
+                        });
+
+                        const orderData = await restClient(`/api/v2/orders?cart_id=${cartId}`, {
+                            method: 'GET',
+                            headers: {
+                                backendTechnology: 'bigcommerce'
+                            }
+                        });
+
+                        setData(getPaymentMethodsParser(paymentOptions, orderData));
+                    } catch (err) {
+                        const { data: checkout } = await restClient(
+                            `/api/v3/checkouts/${cartId}?include=consignments.available_shipping_options`,
+                            {
+                                method: 'GET',
+                                headers: {
+                                    backendTechnology: 'bigcommerce'
+                                }
+                            }
+                        );
+
+                        await restClient(`/api/v3/checkouts/${cartId}/billing-address/${checkout.billing_address.id}`, {
+                            method: 'PUT',
+                            headers: {
+                                backendTechnology: 'bigcommerce'
+                            },
+                            body: JSON.stringify(checkout.consignments[0].address)
+                        });
+
+                        await restClient(`/api/v3/checkouts/${cartId}/consignments/${checkout.consignments[0].id}`, {
+                            method: 'PUT',
+                            headers: {
+                                backendTechnology: 'bigcommerce'
+                            },
+                            body: JSON.stringify({ shipping_option_id: checkout.consignments[0].available_shipping_options[0].id })
+                        });
+
+                        const { data: order } = await restClient(`/api/v3/checkouts/${cartId}/orders`, {
+                            method: 'POST',
+                            headers: {
+                                backendTechnology: 'bigcommerce'
+                            }
+                        });
+
+                        const { data: paymentOptions } = await restClient(`/api/v3/payments/methods?order_id=${order.id}`, {
+                            method: 'GET',
+                            headers: {
+                                backendTechnology: 'bigcommerce'
+                            }
+                        });
+
+                        const orderData = await restClient(`/api/v2/orders?cart_id=${cartId}`, {
+                            method: 'GET',
+                            headers: {
+                                backendTechnology: 'bigcommerce'
+                            }
+                        });
+
+                        setData(getPaymentMethodsParser(paymentOptions, orderData));
+                    }
+                }
+            }
+            setLoading(false);
+        };
+        fetchData();
+    }, []);
+
+    return { data, loading };
 };
 
 export default GetPaymentMethods;
--- src/resolvers/cart/payment/GetPaymentMethods/getPaymentMethodsParser.ts
+++ src/resolvers/cart/payment/GetPaymentMethods/getPaymentMethodsParser.ts
@@ -1,6 +1,28 @@
 import { GetPaymentMethodsQuery } from '@schema';
 
-export const getPaymentMethodsParser = (data: any): GetPaymentMethodsQuery => {
-    // Your parser logic here
-    return data;
+export const getPaymentMethodsParser = (data: any, orderData): GetPaymentMethodsQuery => {
+    const types = {
+        store_credit: 'creditsystem',
+        gift_certificate: 'braintree', // TODO_B2BStore: Create a new file for gift certificate payments. This is MUST be changed
+        card: 'braintree', // TODO_B2BStore: Check how to get the common credit card view
+        braintree: 'braintree',
+        bank_deposit: 'banktransfer'
+    };
+
+    return {
+        cart: {
+            id: orderData[0].cart_id,
+            available_payment_methods: data.map((method) =>
+                types[method.type]
+                    ? {
+                          code: types[method.type],
+                          title: method.name
+                      }
+                    : null
+            ),
+            selected_payment_method: {
+                code: types[orderData[0].payment_method] ? types[orderData[0].payment_method] : ''
+            }
+        }
+    };
 };
