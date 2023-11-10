cd4213c477ff45afc1f783746625b0dc780cadcf
dggorienteed <dgonzalez@orienteed.com>
2023-11-10 10:27:32 +0100

    Fix data setting and network status in getCheckoutDetails resolver

--- src/resolvers/cart/GetCheckoutDetails/getCheckoutDetails.ts
+++ src/resolvers/cart/GetCheckoutDetails/getCheckoutDetails.ts
@@ -4,60 +4,46 @@ import { GetCheckoutDetailsQueryVariables } from '@schema';
 import { getCheckoutDetailsParser } from './getCheckoutDetailsParser';
 import { useEffect, useState } from 'react';
 
-const GetCheckoutDetails = (clientProps: ClientProps) => (resolverProps: GetCheckoutDetailsQueryVariables) => {
-    const { restClient } = clientProps;
-    const { cartId }  = resolverProps;
- 
-    const [loading, setLoading] = useState(true);
-    const [data, setData] = useState<any>(null);
-    const [data2, setData2] = useState<any>(null);
-    const [error, setError] = useState(null);
+const GetCheckoutDetails =
+    (clientProps: ClientProps) =>
+    (resolverProps: GetCheckoutDetailsQueryVariables = { cartId: null }) => {
+        const { restClient } = clientProps;
+        const { cartId } = resolverProps;
 
-    useEffect(() => {
-        const fetchData = async () => {
-            setLoading(true);
-            try {
-                const rawData = await restClient(
-                    `/api/v3/checkouts/${cartId}?include=cart.line_items.physical_items.options,cart.line_items.digital_items.options`,
-                    {
-                        method: 'GET',
-                        headers: {
-                            backendTechnology: 'bigcommerce'
-                        }
+        const [data, setData] = useState(undefined);
+        const [networkStatus, setNetworkStatus] = useState(undefined);
+
+        useEffect(() => {
+            const fetchData = async () => {
+                setNetworkStatus(1); // loading
+                if (cartId) {
+                    try {
+                        const rawData = await restClient(
+                            `/api/v3/checkouts/${cartId}?include=cart.line_items.physical_items.options,cart.line_items.digital_items.options`,
+                            {
+                                method: 'GET',
+                                headers: {
+                                    backendTechnology: 'bigcommerce'
+                                }
+                            }
+                        );
+                        const rawData2 = await restClient(`/api/v2/payments/methods`, {
+                            method: 'GET',
+                            headers: {
+                                backendTechnology: 'bigcommerce'
+                            }
+                        });
+                        setData(getCheckoutDetailsParser(rawData, rawData2));
+                    } catch {
+                        setNetworkStatus(8); // error
                     }
-                );
-                const rawData2 = await restClient(
-                    `/api/v2/payments/methods`,
-                    {
-                        method: 'GET',
-                        headers: {
-                            backendTechnology: 'bigcommerce'
-                        }
-                    }
-                );
-                setData(rawData);
-                setData2(rawData2);
-            } catch (err: any) {
-                setError(err);
-            }
-            setLoading(false);
-        };
-        fetchData();
-    }, []);
+                }
+                setNetworkStatus(7); // ready
+            };
+            fetchData();
+        }, []);
 
-
-    let parsedData = undefined;
-    if (data) {
-        // try {
-        parsedData = getCheckoutDetailsParser(data, data2);
-        // } catch (e) {
-        //     console.error(e);
-        // }
-    }
-
-
-    return { data: parsedData, loading, error };
-
-};
+        return { data, networkStatus };
+    };
 
 export default GetCheckoutDetails;
