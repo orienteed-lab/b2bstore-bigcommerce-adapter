3c073aecc60900fac469da0a4ebdf4e18427c324
dggorienteed <dgonzalez@orienteed.com>
2023-11-15 16:30:54 +0100

    Update setShippingMethod resolver

--- src/resolvers/cart/shipping/SetShippingMethod/setShippingMethod.ts
+++ src/resolvers/cart/shipping/SetShippingMethod/setShippingMethod.ts
@@ -1,10 +1,51 @@
 import { ClientProps } from 'src';
 import { SetShippingMethodMutationVariables } from '@schema';
+import { useState } from 'react';
 
-const SetShippingMethod = (clientProps: ClientProps) => (resolverProps: SetShippingMethodMutationVariables) => {
-    // Look docs for more info about how to fill this function
+interface SetShippingMethodProps extends SetShippingMethodMutationVariables {
+    onSuccess: any;
+    hasOnSuccess: boolean;
+}
 
-    return { data: {}, loading: false, error: undefined };
+const SetShippingMethod = (clientProps: ClientProps) => (resolverProps: SetShippingMethodProps) => {
+    const { restClient } = clientProps;
+    const { hasOnSuccess } = resolverProps;
+    const [called, setCalled] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [error, seterror] = useState(undefined);
+
+    const setShippingMethod = async ({ variables }) => {
+        setLoading(true);
+        setCalled(true);
+
+        try {
+            const { data: cartData } = await restClient(`/api/v3/checkouts/${variables.cartId}`, {
+                method: 'GET',
+                headers: {
+                    backendTechnology: 'bigcommerce'
+                }
+            });
+
+            await restClient(`/api/v3/checkouts/${variables.cartId}/consignments/${cartData.consignments[0].id}`, {
+                method: 'PUT',
+                headers: {
+                    backendTechnology: 'bigcommerce'
+                },
+                body: JSON.stringify({ shipping_option_id: variables.shippingMethod.carrier_code })
+            });
+
+            if (hasOnSuccess) {
+                const { onSuccess } = resolverProps;
+                onSuccess();
+            }
+        } catch (err) {
+            seterror(err);
+        }
+
+        setLoading(false);
+    };
+
+    return { setShippingMethod, setShippingMethodCall: setShippingMethod, loading, called, error };
 };
 
 export default SetShippingMethod;
