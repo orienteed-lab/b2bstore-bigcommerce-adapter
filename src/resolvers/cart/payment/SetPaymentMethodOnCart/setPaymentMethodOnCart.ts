069aa73ca76721abfee4bed4eaf9ce9fdea692c4
dggorienteed <dgonzalez@orienteed.com>
2023-11-21 15:30:19 +0100

    Update setPaymentMethodOnCart resolver

--- src/resolvers/cart/payment/SetPaymentMethodOnCart/setPaymentMethodOnCart.ts
+++ src/resolvers/cart/payment/SetPaymentMethodOnCart/setPaymentMethodOnCart.ts
@@ -1,10 +1,45 @@
 import { ClientProps } from 'src';
 import { SetPaymentMethodOnCartMutationVariables } from '@schema';
+import { useState } from 'react';
 
 const SetPaymentMethodOnCart = (clientProps: ClientProps) => (resolverProps: SetPaymentMethodOnCartMutationVariables) => {
-    // Look docs for more info about how to fill this function
+    const { restClient } = clientProps;
+    const [error, setError] = useState(undefined);
+    const [loading, setLoading] = useState(false);
+    const [called, setCalled] = useState(false);
 
-    return { data: {}, loading: false, error: undefined };
+    const types = {
+        // BigCommerce types: Manual, Credit Card, Cash,Test Payment Gateway, etc.
+        creditsystem: 'store_credit',
+        giftcertificate: 'gift_certificate', // TODO_B2BStore: Create a new file for gift certificate payments. This is MUST be changed
+        braintree: 'card',
+    };
+
+
+    const fetch = async ({ variables }) => {
+        setLoading(true);
+        setCalled(true);
+        try {
+            const orderData = await restClient(`/api/v2/orders?cart_id=${variables.cartId}`, {
+                method: 'GET',
+                headers: {
+                    backendTechnology: 'bigcommerce'
+                }
+            });
+
+            await restClient(`/api/v2/orders/${orderData[0].id}`, {
+                method: 'PUT',
+                headers: {
+                    backendTechnology: 'bigcommerce'
+                },
+                body: JSON.stringify({ payment_method: types[variables.payment_method] })
+            });
+        } catch (err) {
+            setError(err);
+        }
+    };
+
+    return { fetch, loading, error, called };
 };
 
 export default SetPaymentMethodOnCart;
