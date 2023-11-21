e981a40eb6531aa17e71da8a0e72e1e7404cc353
dggorienteed <dgonzalez@orienteed.com>
2023-11-21 15:33:01 +0100

    Update setShippingAddress resolver

--- src/resolvers/cart/shipping/SetShippingAddress/setShippingAddress.ts
+++ src/resolvers/cart/shipping/SetShippingAddress/setShippingAddress.ts
@@ -1,10 +1,123 @@
 import { ClientProps } from 'src';
 import { SetShippingAddressMutationVariables } from '@schema';
+import { useState } from 'react';
+
+import { setShippingAddressParser } from './setShippingAddressParser';
 
 const SetShippingAddress = (clientProps: ClientProps) => (resolverProps: SetShippingAddressMutationVariables) => {
-    // Look docs for more info about how to fill this function
+    const { restClient } = clientProps;
+    const [called, setCalled] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(undefined);
 
-    return { data: {}, loading: false, error: undefined };
+    const setShippingAddress = async ({ variables }) => {
+        setLoading(true);
+        setCalled(true);
+        try {
+            const { data: cartData } = await restClient(`/api/v3/checkouts/${variables.cartId}`, {
+                method: 'GET',
+                headers: {
+                    backendTechnology: 'bigcommerce'
+                }
+            });
+
+            const body = {
+                address: {
+                    email: cartData.cart.email,
+                    city: variables.address.city,
+                    first_name: variables.address.firstname,
+                    last_name: variables.address.lastname,
+                    address1: variables.address.street[0],
+                    phone: variables.address.telephone,
+                    country_code: variables.address.country_code,
+                    postal_code: variables.address.postcode,
+                    state_or_province: variables.address.region
+                }
+            };
+
+            if (cartData.consignments.length === 0) {
+                await restClient(`/v3/checkouts/${variables.cartId}/consignments`, {
+                    method: 'POST',
+                    headers: {
+                        backendTechnology: 'bigcommerce'
+                    },
+                    body: JSON.stringify(body)
+                });
+            } else {
+                await restClient(`/v3/checkouts/${variables.cartId}/consignments/${cartData.consignments[0].id}`, {
+                    method: 'PUT',
+                    headers: {
+                        backendTechnology: 'bigcommerce'
+                    },
+                    body: JSON.stringify(body)
+                });
+            }
+        } catch (err) {
+            setError(err);
+        }
+        setLoading(false);
+    };
+
+    const setShippingAddressOnCart = async ({ variables }) => {
+        let parsedData = undefined;
+        setLoading(true);
+        setCalled(true);
+        try {
+            const { data: cartData } = await restClient(`/api/v3/checkouts/${variables.cartId}`, {
+                method: 'GET',
+                headers: {
+                    backendTechnology: 'bigcommerce'
+                }
+            });
+
+            const body = {
+                address: {
+                    email: cartData.cart.email,
+                    city: variables.city,
+                    first_name: variables.firstname,
+                    last_name: variables.lastname,
+                    address1: variables.street[0],
+                    phone: variables.telephone,
+                    country_code: variables.country_id,
+                    postal_code: variables.postcode,
+                    state_or_province_code: variables.region_code
+                }
+            };
+
+            if (cartData.consignments.length === 0) {
+                await restClient(`/v3/checkouts/${variables.cartId}/consignments`, {
+                    method: 'POST',
+                    headers: {
+                        backendTechnology: 'bigcommerce'
+                    },
+                    body: JSON.stringify(body)
+                });
+            } else {
+                await restClient(`/v3/checkouts/${variables.cartId}/consignments/${cartData.consignments[0].id}`, {
+                    method: 'PUT',
+                    headers: {
+                        backendTechnology: 'bigcommerce'
+                    },
+                    body: JSON.stringify(body)
+                });
+            }
+
+            const { data: newCartData } = await restClient(`/api/v3/checkouts/${variables.cartId}`, {
+                method: 'GET',
+                headers: {
+                    backendTechnology: 'bigcommerce'
+                }
+            });
+
+            parsedData = setShippingAddressParser(newCartData);
+        } catch (err) {
+            setError(err);
+        }
+        setLoading(false);
+        return { data: parsedData };
+    };
+
+    return { setShippingAddress, setShippingAddressOnCart, called, loading, error };
 };
 
 export default SetShippingAddress;
--- src/resolvers/cart/shipping/SetShippingAddress/setShippingAddressParser.ts
+++ src/resolvers/cart/shipping/SetShippingAddress/setShippingAddressParser.ts
@@ -1,6 +1,25 @@
 import { SetShippingAddressMutation } from '@schema';
 
 export const setShippingAddressParser = (data: any): SetShippingAddressMutation => {
-    // Your parser logic here
-    return data;
+    return {
+        setShippingAddressesOnCart: {
+            cart: {
+                id: data.id,
+                total_quantity: data.consignments[0].line_item_ids.length,
+                shipping_addresses: data.consignments.map((cons) => ({
+                    available_shipping_methods: cons.available_shipping_options.map((opt) => ({
+                        available: true, // If it's in the list we understand that is available
+                        carrier_code: '',
+                        carrier_title: '',
+                        method_code: '',
+                        method_title: opt.type,
+                        amount: {
+                            currency: data.cart.currency.code,
+                            value: opt.cost
+                        }
+                    }))
+                }))
+            }
+        }
+    };
 };
