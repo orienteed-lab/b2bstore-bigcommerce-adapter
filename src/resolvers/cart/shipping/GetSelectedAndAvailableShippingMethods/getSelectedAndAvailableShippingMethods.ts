918b326dafb6c7d23f8cd2d21bcff445cd0fe8f6
dggorienteed <dgonzalez@orienteed.com>
2023-11-21 15:31:43 +0100

    Update getSelectedAndAvailablePaymentMethods resolver

--- src/resolvers/cart/shipping/GetSelectedAndAvailableShippingMethods/getSelectedAndAvailableShippingMethods.ts
+++ src/resolvers/cart/shipping/GetSelectedAndAvailableShippingMethods/getSelectedAndAvailableShippingMethods.ts
@@ -1,11 +1,63 @@
 import { ClientProps } from 'src';
 import { GetSelectedAndAvailableShippingMethodsQueryVariables } from '@schema';
+import { useEffect, useState } from 'react';
+
+import { getSelectedAndAvailableShippingMethodsParser } from './getSelectedAndAvailableShippingMethodsParser';
 
 const GetSelectedAndAvailableShippingMethods =
     (clientProps: ClientProps) => (resolverProps: GetSelectedAndAvailableShippingMethodsQueryVariables) => {
-        // Look docs for more info about how to fill this function
+        const { restClient } = clientProps;
+        const { cartId } = resolverProps;
+        const [data, setData] = useState(undefined);
+        const [loading, setLoading] = useState(true);
 
-        return { data: {}, loading: false, error: undefined };
+        useEffect(() => {
+            const fetchData = async () => {
+                setLoading(true);
+                if (cartId) {
+                    let countries = [];
+                    let regions = [];
+                    const { data: checkoutData } = await restClient(
+                        `/api/v3/checkouts/${cartId}?include=consignments.available_shipping_options`,
+                        {
+                            method: 'GET',
+                            headers: {
+                                backendTechnology: 'bigcommerce'
+                            }
+                        }
+                    );
+
+                    for (const cons of checkoutData.consignments) {
+                        const country = await restClient(
+                            `/api/v2/countries?country_iso2=${cons.shipping_address.country_code}`,
+                            {
+                                method: 'GET',
+                                headers: {
+                                    backendTechnology: 'bigcommerce'
+                                }
+                            }
+                        );
+                        countries.push(country[0].country);
+                        const state = await restClient(
+                            `/api/v2/countries/${country[0].id}/states?state_abbreviation=${cons.shipping_address.state_or_province_code}`,
+                            {
+                                method: 'GET',
+                                headers: {
+                                    backendTechnology: 'bigcommerce'
+                                }
+                            }
+                        );
+                        regions.push(state[0].id)
+                    }
+
+                    setData(getSelectedAndAvailableShippingMethodsParser(checkoutData, countries, regions));
+                }
+                setLoading(false);
+            };
+            fetchData();
+        }, []);
+
+        return { data, loading };
     };
 
 export default GetSelectedAndAvailableShippingMethods;
--- src/resolvers/cart/shipping/GetSelectedAndAvailableShippingMethods/getSelectedAndAvailableShippingMethodsParser.ts
+++ src/resolvers/cart/shipping/GetSelectedAndAvailableShippingMethods/getSelectedAndAvailableShippingMethodsParser.ts
@@ -1,6 +1,51 @@
 import { GetSelectedAndAvailableShippingMethodsQuery } from '@schema';
 
-export const getSelectedAndAvailableShippingMethodsParser = (data: any): GetSelectedAndAvailableShippingMethodsQuery => {
-    // Your parser logic here
-    return data;
+export const getSelectedAndAvailableShippingMethodsParser = (data: any, countries: any, regions: any): GetSelectedAndAvailableShippingMethodsQuery => {
+    return {
+        cart: {
+            __typename: 'Cart',
+            id: data.id,
+            email: data.cart.email,
+            shipping_addresses: data.consignments.map((cons, index) => ({
+                __typename: 'ShippingCartAddress',
+                city: cons.shipping_address.city,
+                country: {
+                    code: cons.shipping_address.country_code,
+                    label: countries[index]
+                },
+                firstname: cons.shipping_address.first_name,
+                lastname: cons.shipping_address.last_name,
+                street: [cons.shipping_address.address1, cons.shipping_address.address2],
+                postcode: cons.shipping_address.postal_code,
+                region: {
+                    code: cons.shipping_address.state_or_province_code,
+                    label: cons.shipping_address.state_or_province,
+                    region_id: regions[index]
+                },
+                telephone: cons.shipping_address.phone,
+                available_shipping_methods: cons.available_shipping_options.map((option) => ({
+                    __typename: 'AvailableShippingMethod',
+                    amount: {
+                        currency: data.cart.currency.code,
+                        value: option.cost
+                    },
+                    available: true,
+                    carrier_code: option.id,
+                    carrier_title: '',
+                    method_code: option.type,
+                    method_title: option.description
+                })),
+                selected_shipping_method: {
+                    __typename: 'SelectedShippingMethod',
+                    amount: {
+                        currency: data.cart.currency.code,
+                        value: cons.selected_shipping_option?.cost
+                    },
+                    carrier_code: cons.selected_shipping_option?.id,
+                    method_code: cons.selected_shipping_option?.type,
+                    method_title: cons.selected_shipping_option?.description
+                }
+            }))
+        }
+    };
 };
