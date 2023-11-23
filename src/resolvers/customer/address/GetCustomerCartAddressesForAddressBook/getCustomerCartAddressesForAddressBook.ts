3516142e3832d0896b2403c536c987da5859a161
David Gutierrez <dgutierrez@orienteed.com>
2023-11-23 16:59:38 +0100

    Update getCustomerCartAddressesForAddressBook resolver

--- src/resolvers/customer/address/GetCustomerCartAddressesForAddressBook/getCustomerCartAddressesForAddressBook.ts
+++ src/resolvers/customer/address/GetCustomerCartAddressesForAddressBook/getCustomerCartAddressesForAddressBook.ts
@@ -1,9 +1,47 @@
 import { ClientProps } from 'src';
 
-const GetCustomerCartAddressesForAddressBook = (clientProps: ClientProps) => () => {
-    // Look docs for more info about how to fill this function
+import { useState, useEffect } from 'react';
+import { getCustomerCartAddressesForAddressBookParser } from './getCustomerCartAddressesForAddressBookParser';
 
-    return { data: {}, loading: false, error: undefined };
+interface GetCustomerCartAddressesForAddressBookProps {
+    isSignedIn: boolean,
+    cartId: string
+}
+
+const GetCustomerCartAddressesForAddressBook = (clientProps: ClientProps) => (resolverProps: GetCustomerCartAddressesForAddressBookProps) => {
+    const { restClient } = clientProps;
+    const { cartId } = resolverProps;
+    const { isSignedIn } = resolverProps;
+    const [data, setData] = useState(undefined);
+    const [loading, setLoading] = useState(true);
+
+    console.log("Entrando en getCustomerCartAddresses")
+    console.log(isSignedIn)
+    console.log(cartId)
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            if (isSignedIn && cartId) {
+                const {data: rawData} = await restClient(
+                    `/api/v3/checkouts/${cartId}`,
+                    {
+                        method: 'GET',
+                        headers: {
+                            backendTechnology: 'bigcommerce'
+                        }
+                    }
+                );
+
+                setData(getCustomerCartAddressesForAddressBookParser(rawData));
+            };
+            setLoading(false);
+        };
+        fetchData();
+    }, [])
+
+    console.log(data)
+    return { data, loading };
 };
 
 export default GetCustomerCartAddressesForAddressBook;
--- src/resolvers/customer/address/GetCustomerCartAddressesForAddressBook/getCustomerCartAddressesForAddressBookParser.ts
+++ src/resolvers/customer/address/GetCustomerCartAddressesForAddressBook/getCustomerCartAddressesForAddressBookParser.ts
@@ -1,6 +1,31 @@
 import { GetCustomerCartAddressesForAddressBookQuery } from '@schema';
 
 export const getCustomerCartAddressesForAddressBookParser = (data: any): GetCustomerCartAddressesForAddressBookQuery => {
-    // Your parser logic here
-    return data;
+    return {
+        customerCart: {
+            __typename:'Cart',
+            id: data.id,
+            email: data.cart.email,
+            shipping_addresses: data.consignments.map((ship) => ({
+                __typename: 'ShippingCartAddress',
+                city: ship.shipping_address.city,
+                country: {
+                    __typename: 'CartAddressCountry',
+                    code: ship.shipping_address.country_code,
+                    label: ship.shipping_address.country_code
+                },
+                firstname: ship.shipping_address.first_name,
+                lastname: ship.shipping_address.last_name,
+                postcode: ship.shipping_address.postal_code,
+                region: {
+                    __typename: 'CartAddressRegion',
+                    code: ship.shipping_address.state_or_province_code,
+                    label: ship.shipping_address.state_or_province,
+                    region_id: 1
+                },
+                street: [ship.shipping_address.address1, ship.shipping_address.address2],
+                telephone: ship.shipping_address.phone
+            }))
+        }
+    };
 };
