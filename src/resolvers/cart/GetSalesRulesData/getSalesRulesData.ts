26cea3c0a52aa2042ff42901ba3a44911e18dd84
David Gutierrez <dgutierrez@orienteed.com>
2023-11-28 16:22:22 +0100

    update getSalesRulesData resolver (needs to test)

--- src/resolvers/cart/GetSalesRulesData/getSalesRulesData.ts
+++ src/resolvers/cart/GetSalesRulesData/getSalesRulesData.ts
@@ -1,10 +1,36 @@
 import { ClientProps } from 'src';
 import { GetSalesRulesDataQueryVariables } from '@schema';
+import { useEffect, useState } from 'react';
 
+import { getSalesRulesDataParser } from './getSalesRulesDataParser';
 const GetSalesRulesData = (clientProps: ClientProps) => (resolverProps: GetSalesRulesDataQueryVariables) => {
-    // Look docs for more info about how to fill this function
+    const { restClient } = clientProps;
+    const { cartId } = resolverProps;
 
-    return { data: {}, loading: false, error: undefined };
+    const [data, setData] = useState(undefined);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            if (cartId) {
+                    const rawData = await restClient(
+                        `/api/v3/carts/${cartId}?include=cart.line_items.physical_items.options,cart.line_items.digital_items.options`,
+                        {
+                            method: 'GET',
+                            headers: {
+                                backendTechnology: 'bigcommerce'
+                            }
+                        }
+                    );
+                    setData(getSalesRulesDataParser(rawData.data));
+            };
+            setLoading(false);
+        };
+        fetchData();
+    }, []);
+
+    return { data: data, loading: loading};
 };
 
 export default GetSalesRulesData;
--- src/resolvers/cart/GetSalesRulesData/getSalesRulesDataParser.ts
+++ src/resolvers/cart/GetSalesRulesData/getSalesRulesDataParser.ts
@@ -1,6 +1,110 @@
 import { GetSalesRulesDataQuery } from '@schema';
 
 export const getSalesRulesDataParser = (data: any): GetSalesRulesDataQuery => {
-    // Your parser logic here
-    return data;
-};
+    const total_quantity = () => {
+        var total = 0;
+        if (data.line_items.physical_items) {
+            data.line_items.physical_items.map((item: any) => {
+                total = total + item.quantity;
+            });
+        }
+        if (data.line_items.digital_items) {
+            data.line_items.digital_items.map((item: any) => {
+                total = total + item.quantity;
+            });
+        }
+        if (data.line_items.custom_items) {
+            data.line_items.custom_items.map((item: any) => {
+                total = total + item.quantity;
+            });
+        }
+        return total;
+    };
+
+    const getItems = () => {
+        var items = [];
+
+        if (data.line_items.physical_items) {
+            data.line_items.physical_items.map((item: any) => {
+                item.options.length !== 0
+                    ? items.push({
+                          __typename: 'ConfigurableCartItem',
+                          uid: item.id,
+                          product: {
+                              __typename: 'ConfigurableProduct',
+                              uid: item.id,
+                              weight: item.weight
+                          }
+                      })
+                    : items.push({
+                          __typename: 'SimpleItem',
+                          uid: item.id,
+                          product: {
+                              __typename: 'SimpleProduct',
+                              uid: item.id,
+                              weight: item.weight //The only way to get the weight is doing a request to a Cart, not a checkout
+                          }
+                      });
+            });
+        }
+
+        if (data.line_items.digital_items) {
+            data.line_items.digital_items.map((item: any) => {
+                item.options.length !== 0
+                    ? items.push({
+                          __typename: 'DownloadableCartItem',
+                          uid: item.id,
+                          product: {
+                              __typename: 'DownloadableProduct',
+                              uid: item.id
+                          }
+                      })
+                    : items.push({
+                          __typename: 'DownloadableCartItem',
+                          uid: item.id,
+                          product: {
+                              __typename: 'DownloadableProduct',
+                              uid: item.id
+                          }
+                      });
+            });
+        }
+
+        if (data.line_items.custom_items) {
+            data.line_items.custom_items.map((item: any) => {
+                items.push({
+                    __typename: 'DownloadableCartItem',
+                    uid: item.id,
+                    product: {
+                        __typename: 'VirtualProduct',
+                        uid: item.id
+                    }
+                });
+            });
+        }
+        return items;
+    };
+
+    
+    return {
+        cart: {
+            __typename: 'Cart',
+            id: data.id,
+            total_quantity: total_quantity(),
+            items: getItems(),
+            prices:{
+                __typename:'CartPrices',
+                subtotal_excluding_tax: {
+                    __typename:'Money',
+                    value: data.base_amount
+                },
+                subtotal_including_tax: {
+                    __typename:'Money',
+                    value: data.cart_amount
+                }
+            },
+            selected_payment_method: null, //In Bigcommerce, is impossible to get selected payment method in cart
+            shipping_addresses: null // In BigCommerce, there are no shipping addresses in cart 
+        }
+    };
+}
