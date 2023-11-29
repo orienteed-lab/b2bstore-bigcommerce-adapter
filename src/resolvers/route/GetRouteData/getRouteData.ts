e356ac5d9ab15ec27b5450262a0d33fb97bf43d7
David Gutierrez <dgutierrez@orienteed.com>
2023-11-29 16:41:58 +0100

    update getRouteData resolver

--- src/resolvers/route/GetRouteData/getRouteData.ts
+++ src/resolvers/route/GetRouteData/getRouteData.ts
@@ -1,10 +1,37 @@
 import { ClientProps } from 'src';
 import { GetRouteDataQueryVariables } from '@schema';
 
-const GetRouteData = (clientProps: ClientProps) => (resolverProps: GetRouteDataQueryVariables) => {
-    // Look docs for more info about how to fill this function
+import DEFAULT_OPERATIONS from './getRouteData.gql'
+import { getRouteDataParser } from './getRouteDataParser';
+import { useState } from 'react';
 
-    return { data: {}, loading: false, error: undefined };
+const GetRouteData = (clientProps: ClientProps) => (resolverProps: GetRouteDataQueryVariables) => {
+    const { mergeOperations, useAwaitQuery } = clientProps;
+    const [data, setData] = useState(undefined);
+
+    const { getRouteDataQuery } = mergeOperations(DEFAULT_OPERATIONS);
+    const getPath = useAwaitQuery(getRouteDataQuery);
+
+    const fetchRouteData = async ({variables}) => {
+        let parsedData = undefined;
+        const { data: pathData } = await getPath({
+            context: {
+                headers: {
+                    backendTechnology: ['bigcommerce']
+                }
+            },
+            variables: {
+                path: variables.url.split('.html')[0]
+            }
+        });
+        
+        parsedData = getRouteDataParser(pathData, variables.url);
+        setData(parsedData);
+        
+        return { data: parsedData }
+    }
+
+    return { fetchRouteData };
 };
 
 export default GetRouteData;
--- src/resolvers/route/GetRouteData/getRouteDataParser.ts
+++ src/resolvers/route/GetRouteData/getRouteDataParser.ts
@@ -1,6 +1,22 @@
 import { GetRouteDataQuery } from '@schema';
 
-export const getRouteDataParser = (data: any): GetRouteDataQuery => {
-    // Your parser logic here
-    return data;
+export const getRouteDataParser = (data: any, path: any): GetRouteDataQuery => {
+    if (path === '/') { // It's the Home page
+        return {
+            route: {
+                __typename: 'CmsPage',
+                relative_url: 'home'
+            }
+        };
+    } else {
+        return {
+            route: data.site.route.node.__typename === 'Category' ? { // It's a category
+                __typename: 'CategoryTree',
+                relative_url: `${data.site.route.node.path.slice(1, -1)}.html`,
+            } : data.site.route.node.productOptions.edges.length !== 0 ? { // It's a configurable product
+                __typename: 'ConfigurableProduct',
+                relative_url: `${data.site.route.node.path.slice(1, -1)}.html`,
+            } : null // It's a simple product (the URL is generated in other way)
+        }
+    }
 };
