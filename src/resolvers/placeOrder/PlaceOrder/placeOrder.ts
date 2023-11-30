a11f2df286fbbcd8816f54d21b4c55c6214e98b4
dggorienteed <dgonzalez@orienteed.com>
2023-11-30 11:04:36 +0100

    Add payment processing in placeOrder resolver

--- src/resolvers/placeOrder/PlaceOrder/placeOrder.ts
+++ src/resolvers/placeOrder/PlaceOrder/placeOrder.ts
@@ -10,10 +10,11 @@ const PlaceOrder = (clientProps: ClientProps) => (resolverProps: PlaceOrderMutat
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(undefined);
 
-    const runPlaceOrder = async ({ variables }) => {
+    const runPlaceOrder = async ({ variables, paymentData }) => {
         let parsedData = undefined;
+        let paymentId = undefined;
         setLoading(true);
-        try {
+        // try {
             const orderData = await restClient(`/api/v2/orders?cart_id=${variables.cartId}`, {
                 method: 'GET',
                 headers: {
@@ -21,21 +22,40 @@ const PlaceOrder = (clientProps: ClientProps) => (resolverProps: PlaceOrderMutat
                 }
             });
 
-            parsedData = placeOrderParser(orderData[0].id);
-        } catch (e) {
-            try {
-                const { data: orderData } = await restClient(`/api/v3/checkouts/${variables.cartId}/orders`, {
+            const { data: acceptedPaymentsData } = await restClient(`/api/v3/payments/methods?order_id=${orderData[0].id}`, {
+                method: 'GET',
+                headers: {
+                    backendTechnology: 'bigcommerce'
+                }
+            });
+
+            paymentId = acceptedPaymentsData.find((m) => m.type === paymentData.type)?.id;
+
+            if (paymentId) {
+                const { data: payment } = await restClient(`/api/v3/payments/access_tokens`, {
                     method: 'POST',
                     headers: {
                         backendTechnology: 'bigcommerce'
-                    }
+                    },
+                    body: JSON.stringify({ order: { id: orderData[0].id } })
                 });
 
-                parsedData = placeOrderParser(orderData.id);
-            } catch (err) {
-                setError(err);
+                await restClient(`https://cors-anywhere.herokuapp.com/https://payments.bigcommerce.com/stores/qpq1ivfvi6/payments`, {
+                    method: 'POST',
+                    headers: {
+                        accept: 'application/vnd.bc.v1+json',
+                        authorization: `PAT ${payment.id}`
+                    },
+                    body: JSON.stringify({ payment: { instrument: { type: paymentData.type }, payment_method_id: paymentId } })
+                });
             }
-        }
+
+            parsedData = placeOrderParser(orderData[0].id);
+        // } catch (err) {
+        //     console.log(err)
+        //     setError(err);
+        // }
+
         setData(parsedData);
         setLoading(false);
 
