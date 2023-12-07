45fedcd4d166ede560a6fa302930647848246b83
Omiorienteed <omontenegro@orienteed.com>
2023-12-07 13:22:29 +0100

    Show configurable product options on checkout page

--- src/resolvers/cart/GetItemsInCart/getItemsInCartParser.ts
+++ src/resolvers/cart/GetItemsInCart/getItemsInCartParser.ts
@@ -28,8 +28,45 @@ export const getItemsInCartParser = (data: any): GetItemsInCartQuery => {
             __typename: 'CategoryTree',
             name: categoryName,
         }));
-    
-        return {
+        const object = item.options.length !== 0 ? {
+            __typename: 'ConfigurableCartItem',
+            uid: item.id,
+            quantity: item.quantity,
+            product: {
+                __typename: 'ConfigurableProduct',
+                uid: item.id,
+                sku: item.sku,
+                name: item.name,
+                categories: categories,
+                thumbnail: {
+                    __typename: 'ProductImage',
+                    url: item.image_url 
+                }
+            },
+            prices: {
+                __typename: 'CartItemPrices',
+                price: {
+                    __typename: 'Money',
+                    currency: data.currency.code,
+                    value: item.original_price
+                },
+                row_total: {
+                    __typename: 'Money',
+                    value: item.list_price // TODO_B2B: Check if list price is row total; row total=((ordered item price * ordered item qty) + Tax) - Discount
+                },
+                total_item_discount: {
+                    __typename: 'Money',
+                    value: item.discount_amount
+                }
+            },
+            configurable_options: item.options.map((variant: any) => ({
+                configurable_product_option_uid: variant.name_id,
+                option_label: variant.name,
+                configurable_product_option_value_uid: variant.value_id,
+                value_label: variant.value
+            }))
+        }
+        :{
             __typename: 'SimpleCartItem',
             uid: item.id,
             quantity: item.quantity,
@@ -60,7 +97,8 @@ export const getItemsInCartParser = (data: any): GetItemsInCartQuery => {
                     value: item.discount_amount
                 }
             },
-        };
+        }
+        return object;
     };
     
     const getItems = () => {
