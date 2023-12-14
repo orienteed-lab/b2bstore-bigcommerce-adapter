54fbbf0aac47eb0589c5e7348af4d9a4f9705951
dggorienteed <dgonzalez@orienteed.com>
2023-12-14 11:20:18 +0100

    Update getQuoteList resolver

--- src/resolvers/requestQuote/GetQuoteList/getQuoteList.ts
+++ src/resolvers/requestQuote/GetQuoteList/getQuoteList.ts
@@ -1,10 +1,51 @@
 import { ClientProps } from 'src';
 import { GetQuoteListQueryVariables } from '@schema';
 
-const GetQuoteList = (clientProps: ClientProps) => (resolverProps: GetQuoteListQueryVariables) => {
-    // Look docs for more info about how to fill this function
+import { getQuoteListParser } from './getQuoteListParser';
+import { useEffect, useState } from 'react';
 
-    return { data: {}, loading: false, error: undefined };
+const GetQuoteList = (clientProps: ClientProps) => (resolverProps: GetQuoteListQueryVariables) => {
+    const { restClient } = clientProps;
+    const { pageSize: pageSizeProp, currentPage: currentPageProp } = resolverProps;
+    const [data, setData] = useState(undefined);
+    const [loading, setLoading] = useState(true);
+
+    const refetch = async ({pageSize, currentPage}) => {
+        setLoading(true);
+        try {
+        const { data: quoteList, meta } = await restClient(`/api/v3/io/rfq?limit=${pageSize*currentPage}`, {
+            method: 'GET',
+            headers: {
+                backendTechnology: 'bigcommerceb2b',
+                accept: 'application/json'
+            }
+        });
+
+        const quoteListData = quoteList.slice(pageSize*(currentPage-1), pageSize*currentPage);
+
+        const quotesData = await Promise.all(quoteListData.map(async (quote) => {
+            const { data: quoteData } = await restClient(`/api/v3/io/rfq/${quote.quoteId}?date=${quote.createdAt}`, {
+                method: 'GET',
+                headers: {
+                    backendTechnology: 'bigcommerceb2b'
+                }
+            });
+
+            return quoteData;
+        }))
+
+        setData(getQuoteListParser(quoteListData, meta, quotesData, pageSize, currentPage));
+    } catch (err) {
+        console.log(err);
+    }
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        refetch({pageSize: pageSizeProp, currentPage: currentPageProp})
+    }, [])
+
+    return { data, loading, refetch };
 };
 
 export default GetQuoteList;
--- src/resolvers/requestQuote/GetQuoteList/getQuoteListParser.ts
+++ src/resolvers/requestQuote/GetQuoteList/getQuoteListParser.ts
@@ -1,6 +1,58 @@
 import { GetQuoteListQuery } from '@schema';
 
-export const getQuoteListParser = (data: any): GetQuoteListQuery => {
-    // Your parser logic here
-    return data;
+export const getQuoteListParser = (data: any, meta: any, quotesData: any, pageSize: any, currentPage: any): GetQuoteListQuery => {
+    const status = {
+        0: 'pending',
+        2: 'pending',
+        3: 'pending'
+    };
+
+    return {
+        mpQuoteList: {
+            page_info: {
+                current_page: currentPage,
+                page_size: pageSize,
+                total_pages: Math.floor(meta.pagination.totalCount / pageSize) + 1
+            },
+            items: data.map((quote, index) => ({
+                created_at: quote.createdAt*1000,
+                quote_currency_code: quote.currency.currencyCode,
+                status: status[quote.status],
+                subtotal: quote.subtotal,
+                entity_id: quote.quoteId,
+                expired_at: quote.expiredAt*1000,
+                discount: quotesData[index].discount,
+                items: quotesData[index].productList.map((product) => ({
+                    name: product.productName,
+                    sku: product.sku,
+                    request_price: product.offeredPrice,
+                    qty: product.quantity,
+                    discount: `${product.discount}%`,
+                    prices: {
+                        total_item_discount: {
+                            currency: quotesData[index].currency.currencyCode,
+                            value: product.discount
+                        },
+                        row_total: {
+                            currency: quotesData[index].currency.currencyCode,
+                            value: product.basePrice
+                        },
+                        price: {
+                            currency: quotesData[index].currency.currencyCode,
+                            value: product.basePrice
+                        }
+                    },
+                    configurable_options:
+                        product.options.length !== 0
+                            ? product.options.map((option) => ({
+                                  id: option.optionId,
+                                  option_label: option.optionName,
+                                  value_id: option.optionValue,
+                                  value_label: option.optionLabel
+                              }))
+                            : []
+                }))
+            }))
+        }
+    };
 };
