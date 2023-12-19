5674574157e2abf1525604fc1a784e84a946aedd
dggorienteed <dgonzalez@orienteed.com>
2023-12-19 15:26:07 +0100

    Update addQuoteToCart resolver

--- src/resolvers/requestQuote/AddQuoteToCart/addQuoteToCart.ts
+++ src/resolvers/requestQuote/AddQuoteToCart/addQuoteToCart.ts
@@ -2,9 +2,34 @@ import { ClientProps } from 'src';
 import { AddQuoteToCartMutationVariables } from '@schema';
 
 const AddQuoteToCart = (clientProps: ClientProps) => (resolverProps: AddQuoteToCartMutationVariables) => {
-    // Look docs for more info about how to fill this function
+    const { restClient } = clientProps;
 
-    return { data: {}, loading: false, error: undefined };
+    const addMpQuoteToCart = async ({ variables }) => {
+        const { data: quoteData } = await restClient(`/api/v3/io/rfq/${variables.quoteId}`, {
+            method: 'GET',
+            headers: {
+                backendTechnology: 'bigcommerceb2b'
+            }
+        });
+
+        const body = {
+            line_items: quoteData.productList.map((prod) => ({
+                quantity: prod.quantity,
+                product_id: prod.productId,
+                variant_id: prod.variantId
+            }))
+        };
+
+        await restClient(`/api/v3/carts/${variables.cartId}/items`, {
+            method: 'POST',
+            headers: {
+                backendTechnology: 'bigcommerce'
+            },
+            body: JSON.stringify(body)
+        });
+    };
+
+    return { addMpQuoteToCart };
 };
 
 export default AddQuoteToCart;
