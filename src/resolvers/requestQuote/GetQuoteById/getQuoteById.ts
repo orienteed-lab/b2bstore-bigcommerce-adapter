fdf1850e2aa7e66a853990e3f7fcfc758a34b56d
dggorienteed <dgonzalez@orienteed.com>
2023-12-20 12:25:08 +0100

    Update getQuoteById resolver

--- src/resolvers/requestQuote/GetQuoteById/getQuoteById.ts
+++ src/resolvers/requestQuote/GetQuoteById/getQuoteById.ts
@@ -1,10 +1,32 @@
 import { ClientProps } from 'src';
 import { GetQuoteByIdQueryVariables } from '@schema';
+import { useEffect, useState } from 'react';
+
+import { getQuoteByIdParser } from './getQuoteByIdParser';
 
 const GetQuoteById = (clientProps: ClientProps) => (resolverProps: GetQuoteByIdQueryVariables) => {
-    // Look docs for more info about how to fill this function
+    const { restClient } = clientProps;
+    const { quote_id } = resolverProps;
+    const [data, setData] = useState(undefined);
+    const [loading, setLoading] = useState(true);
 
-    return { data: {}, loading: false, error: undefined };
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            const {data: rawData} = await restClient(`/api/v3/io/rfq/${quote_id}`, {
+                method: 'GET',
+                headers: {
+                    backendTechnology: 'bigcommerceb2b'
+                }
+            });
+
+            setData(getQuoteByIdParser(rawData, quote_id));
+            setLoading(false);
+        };
+        fetchData();
+    }, [])
+
+    return { data, loading };
 };
 
 export default GetQuoteById;
--- src/resolvers/requestQuote/GetQuoteById/getQuoteByIdParser.ts
+++ src/resolvers/requestQuote/GetQuoteById/getQuoteByIdParser.ts
@@ -1,6 +1,46 @@
 import { GetQuoteByIdQuery } from '@schema';
 
-export const getQuoteByIdParser = (data: any): GetQuoteByIdQuery => {
-    // Your parser logic here
-    return data;
+export const getQuoteByIdParser = (data: any, quote_id: any): GetQuoteByIdQuery => {
+    return {
+        mpQuote: {
+            quote: {
+                base_currency_code: data.currency.currencyCode,
+                base_subtotal: data.subtotal,
+                created_at: (data.createdAt*1000).toString(),
+                customer_email: data.contactInfo.email,
+                entity_id: quote_id,
+                items_count: data.productList.length,
+                items_qty: '',
+                subtotal: data.grandTotal,
+                quote_currency_code: data.currency.currencyCode,
+                items: data.productList.map((prod) => ({
+                    id: prod.productId,
+                    quote_id: quote_id,
+                    sku: prod.sku,
+                    qty: prod.quantity,
+                    name: prod.name,
+                    prices: {
+                        row_total: {
+                            currency: data.currency.currencyCode,
+                            value: prod.offeredPrice
+                        }
+                    },
+                    product: {
+                        name: prod.productName,
+                        url_key: prod.productUrl.slice(1, -1),
+                        url_suffix: '.html',
+                        thumbnail: {
+                            url: prod.imageUrl
+                        }
+                    },
+                    configurable_options: prod.options.length !== 0 ? prod.options.map((opt) => ({
+                        id: opt.optionId,
+                        option_label: opt.optionName,
+                        value_id: opt.optionValue,
+                        value_label: opt.optionLabel
+                    })) : []
+                }))
+            }
+        }
+    };
 };
