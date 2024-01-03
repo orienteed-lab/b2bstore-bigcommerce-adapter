1b5518e0f864ca380e4857fabf56ec2993b2f099
dggorienteed <dgonzalez@orienteed.com>
2024-01-03 13:27:17 +0100

    Update restoreSavedCarts resolver

--- src/resolvers/savedCarts/RestoreSavedCarts/restoreSavedCarts.ts
+++ src/resolvers/savedCarts/RestoreSavedCarts/restoreSavedCarts.ts
@@ -2,9 +2,33 @@ import { ClientProps } from 'src';
 import { RestoreSavedCartsMutationVariables } from '@schema';
 
 const RestoreSavedCarts = (clientProps: ClientProps) => (resolverProps: RestoreSavedCartsMutationVariables) => {
-    // Look docs for more info about how to fill this function
+    const { restClient } = clientProps;
+    const { cartId, token } = resolverProps;
 
-    return { data: {}, loading: false, error: undefined };
+    const restoreSaveCart = async () => {
+        const { data: listData } = await restClient(`/api/v3/io/shopping-list/${token}`, {
+            method: 'GET',
+            headers: {
+                backendTechnology: 'bigcommerceb2b'
+            }
+        });
+
+        await restClient(`/api/v3/carts/${cartId}/items`, {
+            method: 'POST',
+            headers: {
+                backendTechnology: 'bigcommerce'
+            },
+            body: JSON.stringify({
+                line_items: listData.items.map((item) => ({
+                    quantity: item.quantity,
+                    product_id: item.productId,
+                    variant_id: item.variantId
+                }))
+            })
+        });
+    };
+
+    return { restoreSaveCart };
 };
 
 export default RestoreSavedCarts;
