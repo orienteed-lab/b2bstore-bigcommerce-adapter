6295d5df2842f36375541d6fe961654dd19953b7
dggorienteed <dgonzalez@orienteed.com>
2024-01-03 16:04:20 +0100

    Update shareSavedCarts resolver

--- src/resolvers/savedCarts/ShareSavedCarts/shareSavedCarts.ts
+++ src/resolvers/savedCarts/ShareSavedCarts/shareSavedCarts.ts
@@ -2,9 +2,34 @@ import { ClientProps } from 'src';
 import { ShareSavedCartsMutationVariables } from '@schema';
 
 const ShareSavedCarts = (clientProps: ClientProps) => (resolverProps: ShareSavedCartsMutationVariables) => {
-    // Look docs for more info about how to fill this function
+    const { restClient } = clientProps;
 
-    return { data: {}, loading: false, error: undefined };
+    const getShareCart = async ({variables}) => {
+        const { data: listData } = await restClient(`/api/v3/io/shopping-list/${variables.token}`, {
+            method: 'GET',
+            headers: {
+                backendTechnology: 'bigcommerceb2b'
+            }
+        });
+
+        const { data: cartData } = await restClient(`/api/v3/carts/${variables.cartId}/items`, {
+            method: 'POST',
+            headers: {
+                backendTechnology: 'bigcommerce'
+            },
+            body: JSON.stringify({
+                line_items: listData.items.map((item) => ({
+                    quantity: item.quantity,
+                    product_id: item.productId,
+                    variant_id: item.variantId
+                }))
+            })
+        });
+
+        return { data: { mpSaveCartShareCart: !!cartData.id } };
+    };
+
+    return { getShareCart };
 };
 
 export default ShareSavedCarts;
